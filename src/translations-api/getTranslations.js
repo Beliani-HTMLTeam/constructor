f4ab0e88073a3352c75921be99716c32e0c92467
kkazzann <contact@kznlabs.com>
2025-10-28 13:40:02 +0100

    fix: update initStaticTranslations() with a Promise.all()

--- src/translations-api/getTranslations.js
+++ src/translations-api/getTranslations.js
@@ -8,11 +8,18 @@ export let staticTranslations = {
   category_titles: {},
 };
 
-export default function initStaticTranslations() {
-  Object.keys(staticTranslations).forEach(async (key) => {
-    const translations = await getStaticTranslation({ sheet: key });
-    staticTranslations[key] = translations.data;
-  });
+export default async function initStaticTranslations() {
+  await Promise.all(
+    Object.keys(staticTranslations).map(async (key) => {
+      const translations = await getStaticTranslation({ sheet: key });
+
+      // console.log(`Translations for ${key}:`, translations);
+      staticTranslations[key] = translations.data;
+    })
+  );
+
+  // check if translations are present:
+  // console.log(staticTranslations);
 }
 
 initStaticTranslations();
@@ -20,7 +27,7 @@ initStaticTranslations();
 // todo: make alternative functions to get translations directly from Google Sheets API
 
 async function getStaticTranslation({ sheet }) {
-  const url = `${c.api_url}static/${sheet}/`;
+  const url = `${c.api_url}static/${sheet}`;
   const headers = {
     Accept: 'application/json',
     'Content-Type': 'application/json',
