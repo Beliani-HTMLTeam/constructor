439f9578eed9e2c8f553220b6ec16ae4ef3fc313
kkazzann <kamil.kazaniecki@beliani.net>
2025-11-18 09:18:00 +0100

    feat: create advent calendar campaign, update template to match new constructor

---
+++ campaigns/KamilK/004_2025-03-12-advent-calendar-2025.js
@@ -0,0 +1,72 @@
+// Campaign generated from form
+const campaignTranslationsSheet = '03.12.25 - Advent calendar';
+
+const tableQueries = [
+  {
+    tableRange: '17',
+    name: 'intro',
+  },
+  {
+    tableRange: '18:20',
+    name: 'condition',
+  },
+];
+
+const links = {
+  TopImageTitle_href: translateLink({ value: 'content/lp25-12-03' }),
+  TopImageTitle_src: translateImage({ value: '20251203_01.png' }),
+  TopImage: getImageUrl('20251203_Gif.gif', true),
+  Banner_1: translateLink({ value: 'content/lp25-10-23' }),
+  Banner_1_Image: translateImage({ value: '20251023b.png' }),
+  Banner_2: translateLink({ value: 'content/lp25-10-15' }),
+  Banner_2_Image: translateImage({ value: '20251015b.png' }),
+};
+
+const TopImageTitle_data = {
+  color: '#000000',
+  backgroundColor: '#FFCCB7',
+  type: 'twoSameLines',
+};
+
+export default new entities.Campaign({
+  startId: '38727',
+  name: 'Advent Calendar 2025',
+  date: '03.12.2025',
+  issueCardId: '422465',
+  lpId: '27653',
+  alarm: {
+    isActive: false,
+  },
+  isArchive: false,
+  optimizeImg: false,
+  templates: [
+    {
+      name: 'Newsletter',
+      type: types.NEWSLETTER,
+      template: templates.AdventCalendar, // User should change this
+      css: types.CSS.NS,
+      translationsSpreadsheet: campaignTranslationsSheet,
+      background: '#FFCCB7',
+      color: '#000000',
+      wrapper: types.WRAPPER,
+      TopImageTitle_data: TopImageTitle_data,
+      // categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+    },
+    {
+      name: 'Landing',
+      type: types.LANDINGPAGE,
+      template: null, // User should change this
+      css: types.CSS.LP,
+      background: '#FFCCB7',
+      color: '#000000',
+      translationsSpreadsheet: campaignTranslationsSheet,
+      TopImageTitle_data: TopImageTitle_data,
+      // categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+    },
+  ],
+});
+
