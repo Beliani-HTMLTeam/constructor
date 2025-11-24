ecb0ee553edec2d52e694fe808b754be027c1e84
kkazzann <kamil.kazaniecki@beliani.net>
2025-11-24 15:20:24 +0100

    refactor: move unused templates to archive

---
+++ src/templates/archive/index.js
@@ -0,0 +1,96 @@
+import { newsletter_FGG } from '@/templates/archive/newsletter FGG.js';
+import { landing_FGG } from '@/templates/archive/landing FGG.js';
+import { AdventCalendar } from '@/templates/AdventCalendar.js';
+import { CategoriesRowsOfferPart } from '@/templates/archive/CategoriesRowsOfferPart.js';
+import { CategoriesGridOffertPart } from '@/templates/archive/CategoriesGridOffertPart.js';
+import { CategoriesGrid } from '@/templates/archive/CategoriesGrid.js';
+import { mondayOfferFreeebiesLines } from '@/templates/archive/mondayOfferFreeebiesLines.js';
+import { mondayOfferFreeebies } from '@/templates/archive/mondayOfferFreeebies.js';
+import { mondayCategories } from '@/templates/archive/mondayCategories.js';
+import { CategoriesRows } from '@/templates/archive/CategoriesRows.js';
+import { CategoriesRowsLinePriceTitle } from '@/templates/archive/CategoriesRowsLinePriceTitle.js';
+import { CategoriesRowsLine } from '@/templates/archive/CategoriesRowsLine.js';
+import { mondayOfferFreebiesInsideBanner } from '@/templates/archive/mondayOfferFreebiesInsideBanner.js';
+import { Newyear } from '@/templates/archive/Newyear.js';
+import { NewyearLP } from '@/templates/archive/NewyearLP.js';
+import { Moonlight } from '@/templates/archive/Moonlight.js';
+import { MoonlightLP } from '@/templates/archive/MoonlightLP.js';
+import { Gatherings } from '@/templates/archive/Gatherings.js';
+import { GatheringsLP } from '@/templates/archive/GatheringsLP.js';
+import { Modular } from '@/templates/archive/Modular.js';
+import { ModularLP } from '@/templates/archive/ModularLP.js';
+import { Armchairs } from '@/templates/archive/Armchairs.js';
+import { ArmchairsLP } from '@/templates/archive/ArmchairsLP.js';
+import { Home } from '@/templates/archive/Home.js';
+import { HomeLP } from '@/templates/archive/HomeLP.js';
+import { Hallway } from '@/templates/archive/Hallway.js';
+import { HallwayLP } from '@/templates/archive/HallwayLP.js';
+import { Rugs } from '@/templates/archive/Rugs.js';
+import { Living } from '@/templates/archive/Living.js';
+import { Office } from '@/templates/archive/Office.js';
+import { RegularWednesdayNslt } from '@/templates/archive/RegularWednesdayNslt.js';
+// import { RegularWednesdayNslt } from "@templates/RegularWednesdayNslt.js";
+import { FrenchDays } from '@/templates/archive/French.js';
+import { GardenMayNL } from '@/templates/archive/0705nl.js';
+import { GardenMayLP } from '@/templates/archive/0705lp.js';
+import { GardenNL } from '@/templates/archive/GardenNL.js';
+import { GardenLP } from '@/templates/archive/GardenLP.js';
+import { BirthdayNL } from '@/templates/archive/Birthday.js';
+import { ParasolNL } from '@/templates/archive/ParasolNL.js';
+import { ParasolLP } from '@/templates/archive/ParasolLP.js';
+import { BirthdayWhiteNL } from '@/templates/archive/BirthdayWhite.js';
+import { Side } from '@/templates/archive/Side.js';
+import { SideLP } from '@/templates/archive/SideLP.js';
+import { mondayRegularNslt } from '@/templates/archive/mondayRegularNslt.js';
+import { fridayDziwnyNsltr } from '@/templates/archive/fridayDziwnyNsltr.js';
+import { product_of_the_month } from '@/templates/archive/product_of_the_month.js';
+import { Blackweek } from '@/templates/Blackweek.js'
+
+export const templates = {
+  Blackweek,
+  newsletter_FGG,
+  product_of_the_month,
+  mondayRegularNslt,
+  fridayDziwnyNsltr,
+  landing_FGG,
+  AdventCalendar,
+  CategoriesRowsOfferPart,
+  CategoriesGridOffertPart,
+  CategoriesGrid,
+  mondayOfferFreeebiesLines,
+  mondayOfferFreeebies,
+  mondayCategories,
+  CategoriesRows,
+  mondayOfferFreebiesInsideBanner,
+  CategoriesRowsLinePriceTitle,
+  CategoriesRowsLine,
+  Newyear,
+  NewyearLP,
+  Moonlight,
+  MoonlightLP,
+  Gatherings,
+  GatheringsLP,
+  Modular,
+  ModularLP,
+  Armchairs,
+  ArmchairsLP,
+  Home,
+  HomeLP,
+  Hallway,
+  HallwayLP,
+  Rugs,
+  Living,
+  Office,
+  RegularWednesdayNslt,
+  FrenchDays,
+  GardenMayNL,
+  GardenMayLP,
+  GardenNL,
+  GardenLP,
+  BirthdayNL,
+  ParasolNL,
+  ParasolLP,
+  BirthdayWhiteNL,
+  Side,
+  SideLP,
+};
