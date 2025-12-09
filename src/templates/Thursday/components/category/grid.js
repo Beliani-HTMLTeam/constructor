0436285921706d715f9903e79563b06eded80378
kkazzann <kamil.kazaniecki@beliani.net>
2025-12-09 13:31:37 +0100

    fix: fixes missing colors in outlook, campaign table queries

--- src/templates/Thursday/components/category/grid.js
+++ src/templates/Thursday/components/category/grid.js
@@ -1,13 +1,13 @@
 import { Product } from '../Product.js';
 
-export const render = (products, showPrices, showNames, queries, insideContainer = true) => {
+export const render = (products, showPrices, showNames, queries, insideContainer = true, color) => {
   let productsInnerHtml = '';
 
   if (Array.isArray(products)) {
     const cols = 2;
     productsInnerHtml += `
     <tr>
-      <td ${insideContainer ? 'class="newsletterContainer"' : ''}>
+      <td style="color: ${color}" ${insideContainer ? 'class="newsletterContainer"' : ''}>
         <table cellspacing="0" cellpadding="0" border="0" width="100%">`;
 
     for (let i = 0; i < products.length; i += cols) {
@@ -16,10 +16,10 @@ export const render = (products, showPrices, showNames, queries, insideContainer
       for (let c = 0; c < cols; c++) {
         const product = products[i + c];
         // prettier-ignore
-        productsInnerHtml += `<td style="width:50%;vertical-align:top;" width="50%" ${(c + 1) % 2 !== 0 ? 'class="newsletterRight10px"' : 'class="newsletterLeft10px"'}>`;
+        productsInnerHtml += `<td style="color: ${color}; width:50%;vertical-align:top;" width="50%" ${(c + 1) % 2 !== 0 ? 'class="newsletterRight10px"' : 'class="newsletterLeft10px"'}>`;
 
         if (product) {
-          productsInnerHtml += Product(product, showPrices, showNames);
+          productsInnerHtml += Product(product, showPrices, showNames, color);
         }
 
         productsInnerHtml += '</td>';
