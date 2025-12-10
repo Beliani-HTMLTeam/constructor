ce608e2c5b9f352bf58e7ff619d24e3a44b5925d
kkazzann <kamil.kazaniecki@beliani.net>
2025-12-10 09:35:43 +0100

    fix: merge footer and templates changes

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,11 +1,8 @@
 import { Blackweek } from '@/templates/Blackweek.js';
 import { AdventCalendar } from '@/templates/AdventCalendar.js';
 import { Thursday } from '@/templates/Thursday/template.js';
-<<<<<<< HEAD
-=======
 import { MichalTemporary } from '@/templates/MichalTemporary/MichalTemporary.js';
 import { MichalTemporaryLP } from '@/templates/MichalTemporary/MichalTemporaryLP.js';
->>>>>>> 538cc932993c96978bebad799480eb3f59581f9a
 import { FridayEolProducts } from './FridayEolProducts';  
 
 export const templates = {
@@ -13,9 +10,6 @@ export const templates = {
   AdventCalendar,
   Thursday,
   FridayEolProducts,
-<<<<<<< HEAD
-=======
   MichalTemporary,
   MichalTemporaryLP
->>>>>>> 538cc932993c96978bebad799480eb3f59581f9a
 };
