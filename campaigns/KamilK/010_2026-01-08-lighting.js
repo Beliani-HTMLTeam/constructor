07f876334b411ba93f48878720ead6df49d695d1
kkazzann <kamil.kazaniecki@beliani.net>
2025-12-16 14:27:07 +0100

    feat: add new campaign (08.01.2026)

---
+++ campaigns/KamilK/010_2026-01-08-lighting.js
@@ -0,0 +1,327 @@
+// Campaign generated from form
+const campaignTranslationsSheet = '2026::08.01.26 - Lighting';
+
+const tableQueries = [
+  {
+    tableRange: '13:14',
+    name: 'TopImageTitle',
+  },
+  {
+    tableRange: '16',
+    name: 'intro',
+  },
+  {
+    tableRange: '21:22',
+    name: 'condition',
+  },
+  {
+    tableRange: '15:16',
+    name: 'timer',
+    tableName: '2026::Voucher - 05.01.26 - Free duvet cover',
+  },
+];
+
+const links = {
+  TopImageTitle_href: translateLink({ value: 'content/lp26-01-08' }),
+  TopImageTitle_src: translateImage({ value: '20260108_01.png' }),
+
+  TopImage_src: getImageUrl('20260108_Gif.gif', true),
+  TopImage_href: translateLink({ value: 'content/lp26-01-08' }),
+
+  Banner_1: translateLink({ value: 'content/lp25-01-31' }),
+  Banner_1_Image: translateImage({ value: '20250131b.png' }),
+
+  Banner_2: translateLink({ value: 'content/lp26-01-01' }),
+  Banner_2_Image: translateImage({ value: '20260101b.png' }),
+
+  Timer_href: translateLink({ value: 'content/lp26-01-05' }),
+};
+
+const TopImageTitle_data = {
+  color: '#000000',
+  backgroundColor: '#FFD8A6',
+  type: 'twoSameLines',
+};
+
+const categories = [
+  {
+    name: 'Floor Lamps',
+    src: getImageUrl('20260108_Cat00.gif', true),
+    href: 'https://www.beliani.ch/lighting/floor-lamps/',
+    background: '#FFD8A6',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '35',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // THAMES
+      {
+        id: '37149',
+        src: getImageUrl('20260108_Pic01.png', true),
+      },
+      // PENTON
+      {
+        id: '343953',
+        src: getImageUrl('20260108_Pic02.png', true),
+      },
+      // JAVARI
+      {
+        id: '399266',
+        src: getImageUrl('20260108_Pic03.png', true),
+      },
+      // SEVERN
+      {
+        id: '78464',
+        src: getImageUrl('20260108_Pic04.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Table & Bedside Lamps',
+    src: getImageUrl('20260108_Cat00.gif', true),
+    href: 'https://www.beliani.ch/lighting/table-lamps/',
+    background: '#B37373',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '35',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // TULUN
+      {
+        id: '526167',
+        src: getImageUrl('20260108_Pic01.png', true),
+      },
+      // BALUARTE
+      {
+        id: '458785',
+        src: getImageUrl('20260108_Pic02.png', true),
+      },
+      // ZARIMA
+      {
+        id: '315313',
+        src: getImageUrl('20260108_Pic03.png', true),
+      },
+      // VILAR
+      {
+        id: '443134',
+        src: getImageUrl('20260108_Pic04.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Wall Lamps',
+    src: getImageUrl('20260108_Cat00.gif', true),
+    href: 'https://www.beliani.ch/lighting/wall-lamps/',
+    background: '#FFD8A6',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '35',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // LOVAT
+      {
+        id: '194079',
+        src: getImageUrl('20260108_Pic01.png', true),
+      },
+      // TULIJA
+      {
+        id: '397160',
+        src: getImageUrl('20260108_Pic02.png', true),
+      },
+      // JAMMA
+      {
+        id: '447950',
+        src: getImageUrl('20260108_Pic03.png', true),
+      },
+      // MOKVI
+      {
+        id: '420272',
+        src: getImageUrl('20260108_Pic04.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Decorative Lighting',
+    src: getImageUrl('20260108_Cat00.gif', true),
+    href: 'https://www.beliani.ch/lighting/decorative-lighting/',
+    background: '#B37373',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '35',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // MUSONE
+      {
+        id: '69649',
+        src: getImageUrl('20260108_Pic01.png', true),
+      },
+      // PROCYON
+      {
+        id: '429055',
+        src: getImageUrl('20260108_Pic02.png', true),
+      },
+      // FLORA
+      {
+        id: '367074',
+        src: getImageUrl('20260108_Pic03.png', true),
+      },
+      // HADAR
+      {
+        id: '429089',
+        src: getImageUrl('20260108_Pic04.png', true),
+      },
+    ],
+  },
+];
+
+const timer = {
+  freebies: getImageUrl('20260108_free.png', true),
+  image: {
+    CHDE: 'https://gen.sendtric.com/countdown/sw0f5qbl32',
+    CHFR: 'https://gen.sendtric.com/countdown/1xzhngpodf',
+    FR: 'https://gen.sendtric.com/countdown/h1yzod4cme',
+    DE: 'https://gen.sendtric.com/countdown/nm6c9z86df',
+    UK: 'https://gen.sendtric.com/countdown/ilwhx0breu',
+    AT: 'https://gen.sendtric.com/countdown/jczaq10q52',
+    ES: 'https://gen.sendtric.com/countdown/ncxh1y0nbv',
+    PL: 'https://gen.sendtric.com/countdown/o4on8h66ly',
+    NL: 'https://gen.sendtric.com/countdown/fg7pvcrz4g',
+    PT: 'https://gen.sendtric.com/countdown/7xynup71a9',
+    IT: 'https://gen.sendtric.com/countdown/b3y7myqebh',
+    SE: 'https://gen.sendtric.com/countdown/brj7jva6dn',
+    HU: 'https://gen.sendtric.com/countdown/xbkzauobsn',
+    DK: 'https://gen.sendtric.com/countdown/hdfqymvef5',
+    CZ: 'https://gen.sendtric.com/countdown/x1dk1ec3ar',
+    FI: 'https://gen.sendtric.com/countdown/zi7xycdkrq',
+    NO: 'https://gen.sendtric.com/countdown/lje7y7wsw7',
+    SK: 'https://gen.sendtric.com/countdown/mi7qz9wp1i',
+    BENL: 'https://gen.sendtric.com/countdown/0uqpfp4gyc',
+    BEFR: 'https://gen.sendtric.com/countdown/s6dpb00gf7',
+    RO: 'https://gen.sendtric.com/countdown/1nd6lzz3nh',
+  },
+};
+
+export default new entities.Campaign({
+  startId: '39628',
+  name: 'Lighting',
+  date: '08.01.2026',
+  issueCardId: '434481',
+  lpId: '28116',
+  alarm: {
+    isActive: false,
+  },
+  isArchive: false,
+  optimizeImg: false,
+  figmaUrl:
+    'https://www.figma.com/design/bQNokqtNDvCKI7KBPOoSEU/COPY-%7C-2026.01.08---Lighting?node-id=0-1&t=yYirct5tpb9cTUlJ-1',
+  templates: [
+    {
+      name: 'Newsletter',
+      type: types.NEWSLETTER,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.NS,
+      translationsSpreadsheet: campaignTranslationsSheet,
+      background: '#FFD8A6',
+      color: '#000000',
+      wrapper: types.WRAPPER,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      timer: timer,
+      intro: {
+        color: '#000000', // unused
+        backgroundColor: '#FEC273', // unused
+        alignment: 'left',
+        type: 'paragraph',
+      },
+      Inside: {
+        color: '#000000',
+        backgroundColor: '#FFCCB7',
+        type: 'timer',
+      },
+    },
+    {
+      name: 'Landing',
+      type: types.LANDINGPAGE,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.LP,
+      background: '#FFD8A6',
+      color: '#000000',
+      translationsSpreadsheet: campaignTranslationsSheet,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      timer: timer,
+      intro: {
+        color: '#000000', // unused
+        backgroundColor: '#FEC273', // unused
+        alignment: 'left',
+        type: 'paragraph',
+      },
+      Inside: {
+        color: '#000000',
+        backgroundColor: '#FFCCB7',
+        type: 'timer',
+      },
+    },
+  ],
+});
+
