ed65f2cc23cc65cdac35210a5c986e2a6548d7a1
kkazzann <kamil.kazaniecki@beliani.net>
2025-12-29 10:37:13 +0100

    refactor(swapImage): improve image format handling and maintain query parameters

--- src/helpers/swapImage.js
+++ src/helpers/swapImage.js
@@ -1,11 +1,14 @@
 export const swapImage = ({ format, src }) => {
   try {
-    const parsed = new URL(src);
-    const split = parsed.pathname.split('.');
-    if (split[1] === 'gif') {
-      return src;
-    }
-    return parsed.origin + split[0] + format;
+    // images might be versioned eg. ?ver=2137 to prevent CF caching
+    const [base, query] = src.split('?', 2);
+
+    // we don't want to touch gifss
+    if (/\.gif$/i.test(base)) return src;
+
+    const replaced = base.replace(/\.[a-zA-Z0-9]+$/, format);
+
+    return query ? `${replaced}?${query}` : replaced;
   } catch (error) {
     throw new Error(error);
   }
