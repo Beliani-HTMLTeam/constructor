9c2de8479f715abf851a79979de55574a64f16be
ShadowDrake21 <dimka670020040@gmail.com>
2026-01-29 10:55:51 +0100

    Dima's Father's Day GG Campaign

---
+++ src/templates/FathersDayGG/components/components_remake/Product.js
@@ -0,0 +1,62 @@
+import { ImageWithLink } from "./ImageWithLink.js";
+
+export function Product(product, align = "left", style, showPrices, showName) {
+  return `
+  <table cellspacing="0" cellpadding="0" style="width: 100%; ${style ?? ""}">
+    <tbody>
+      <tr>
+        <td>
+          ${
+            "src" in product && product.src !== null ?
+            `<table class="newsletterProductImage" cellspacing="0" cellpadding="0" style="width: 100%; ">
+            <tbody>
+              <tr>
+                <td align="${align}" class="newsletterBottom20px">
+                  ${ImageWithLink({
+                    href: product.href,
+                    src: product.src,
+                    alt: product.name,
+                  })}
+                </td>
+              </tr>
+            </tbody>
+          </table>`
+            :
+            ""
+          }
+          ${
+            showName
+              ? `
+            <table cellspacing="0" cellpadding="0" class="table-layout: fixed;" style="width: 100%; ">
+            <tbody>
+              <tr>
+                <td align="${align}">
+                  <span class="newsletterProductTitle" style="${style ?? ''}">${product.name}</span>
+                </td>
+              </tr>
+                <tr>
+                  <td class="newsletterProductPrices" align="${align}" style="padding-bottom: 8px;">
+                    <span class="newsletterProductLowPrice" style="${style ?? ''} ${
+                  showPrices ? '' : ';text-decoration: none !important;'
+                } ${showPrices ? '' : 'display: none;'}">
+                      ${showPrices ? product.lowPrice : ''}
+                    </span>
+                    ${showPrices && product.highPrice ? '&nbsp;' : ''}
+                    <span class="newsletterProductHightPrice" style="${style ?? ''} ${
+                  showPrices ? '' : ';text-decoration: none !important;'
+                } ${showPrices ? '' : 'display: none;'}">
+                      ${showPrices ? product.highPrice : ''}
+                    </span>
+                  </td>
+                </tr>
+            </tbody>
+          </table>
+            `
+              : `<table cellspacing="0" cellpadding="0" style="width: 100%; "><tr><td></td></tr></table>`
+          }
+        </td>
+      </tr>
+    </tbody>
+  </table>
+    `;
+}
