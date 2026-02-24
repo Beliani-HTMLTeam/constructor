34edb9fe690bb07f97dd6bab486282f89b51f5fb
kkazzann <kamil.kazaniecki@beliani.net>
2026-02-24 08:05:24 +0100

    feat: added 05.03 bbq prep camp

---
+++ campaigns/KamilK/021_2026-03-05-bbq-prep.js
@@ -0,0 +1,333 @@
+// Campaign generated from form
+const campaignTranslationsSheet = '2026::05.03.26 - BBQ prep';
+
+const tableQueries = [
+  {
+    tableRange: '17:18',
+    name: 'TopImageTitle',
+  },
+  {
+    tableRange: '20',
+    name: 'intro',
+  },
+  {
+    tableRange: '25:28',
+    name: 'paragraphs',
+  },
+  {
+    tableRange: '21:24',
+    name: 'categories',
+  },
+  {
+    tableRange: '30:31',
+    name: 'condition',
+  },
+  {
+    tableRange: '15:16',
+    name: 'timer',
+    tableName: '2026::Voucher - 03.03.26 - Free vase	',
+  },
+];
+
+const links = {
+  TopImageTitle_href: translateLink({ value: 'content/lp26-03-05' }),
+  TopImageTitle_src: translateImage({ value: '20260305_01.png' }),
+
+  TopImage_src: getImageUrl('20260305_Pic.png', true),
+  TopImage_href: translateLink({ value: 'content/lp26-03-05' }),
+
+  Banner_1: translateLink({ value: 'content/lp26-02-26' }),
+  Banner_1_Image: translateImage({ value: '20260226b.png' }),
+
+  Banner_2: translateLink({ value: 'content/lp26-02-25' }),
+  Banner_2_Image: translateImage({ value: '20260225b.png' }),
+
+  Timer_href: translateLink({ value: 'content/lp26-03-03' }),
+};
+
+const TopImageTitle_data = {
+  color: '#000000',
+  backgroundColor: '#FD9000',
+  type: 'twoSameLines',
+};
+
+const categories = [
+  {
+    name: 'Garden tables',
+    src: getImageUrl('20260305_Cat00.png', true),
+    href: 'https://www.beliani.ch/outdoor-furniture/garden-tables/',
+    background: '#FD9000',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '60',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // TOLENTINO
+      {
+        id: '682080',
+        src: getImageUrl('20260305_Pic01.png', true),
+      },
+      // SOVARETO
+      {
+        id: '652003',
+        src: getImageUrl('20260305_Pic02.png', true),
+      },
+      // BINASCO
+      {
+        id: '702265',
+        src: getImageUrl('20260305_Pic03.png', true),
+      },
+      // JACO
+      {
+        id: '699961',
+        src: getImageUrl('20260305_Pic04.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Garden chairs',
+    src: getImageUrl('20260305_Cat10.png', true),
+    href: 'https://www.beliani.ch/outdoor-furniture/garden-chairs/',
+    background: '#750000',
+    color: '#ffffff',
+    type: 'grid',
+    cta: true,
+    paddingTop: '35',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // THASOS
+      {
+        id: '700556',
+        src: getImageUrl('20260305_Pic11.png', true),
+      },
+      // SUMIRAGO
+      {
+        id: '700018',
+        src: getImageUrl('20260305_Pic12.png', true),
+      },
+      // MILOS
+      {
+        id: '699345',
+        src: getImageUrl('20260305_Pic13.png', true),
+      },
+      // TIANO
+      {
+        id: '554446',
+        src: getImageUrl('20260305_Pic14.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Fire pits & grills',
+    src: getImageUrl('20260305_Cat20.png', true),
+    href: 'https://www.beliani.ch/garden-accessories/fire-pits/',
+    background: '#FD9000',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '35',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // KAMADO
+      {
+        id: '650888',
+        src: getImageUrl('20260305_Pic21.png', true),
+      },
+      // MARAPI
+      {
+        id: '315877',
+        src: getImageUrl('20260305_Pic22.png', true),
+      },
+      // PAVLOF
+      {
+        id: '301788',
+        src: getImageUrl('20260305_Pic23.png', true),
+      },
+      // TANARA
+      {
+        id: '197909',
+        src: getImageUrl('20260305_Pic24.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Outdoor kitchen',
+    src: getImageUrl('20260305_Cat30.png', true),
+    href: 'https://www.beliani.ch/outdoor-furniture/outdoor-kitchen/',
+    background: '#750000',
+    color: '#ffffff',
+    type: 'grid',
+    cta: true,
+    paddingTop: '35',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // MAENZA blue
+      {
+        id: '701517',
+        src: getImageUrl('20260305_Pic31.png', true),
+      },
+      // MAENZA green
+      {
+        id: '702037',
+        src: getImageUrl('20260305_Pic32.png', true),
+      },
+      // SCANNO
+      {
+        id: '680308',
+        src: getImageUrl('20260305_Pic33.png', true),
+      },
+      // ORTISEI
+      {
+        id: '526467',
+        src: getImageUrl('20260305_Pic34.png', true),
+      },
+    ],
+  },
+];
+
+const timer = {
+  freebies: getImageUrl('20260305free.png', true),
+  image: {
+    CHDE: 'https://gen.sendtric.com/countdown/8qng6y8jf9',
+    CHFR: 'https://gen.sendtric.com/countdown/delzuyel1a',
+    FR: 'https://gen.sendtric.com/countdown/f77s1zv59r',
+    DE: 'https://gen.sendtric.com/countdown/s4fm3p3lx7',
+    UK: 'https://gen.sendtric.com/countdown/qr7nnug7ex',
+    AT: 'https://gen.sendtric.com/countdown/o1tavrswdf',
+    ES: 'https://gen.sendtric.com/countdown/cj3ewlnnmi',
+    PL: 'https://gen.sendtric.com/countdown/lj64ipar23',
+    NL: 'https://gen.sendtric.com/countdown/q42ocebofn',
+    PT: 'https://gen.sendtric.com/countdown/snexfrh72y',
+    IT: 'https://gen.sendtric.com/countdown/ohwzlrfx0w',
+    SE: 'https://gen.sendtric.com/countdown/mu7umhpgfq',
+    HU: 'https://gen.sendtric.com/countdown/r5rny7rhri',
+    DK: 'https://gen.sendtric.com/countdown/78fzhvnwg0',
+    CZ: 'https://gen.sendtric.com/countdown/s94zbd3w67',
+    FI: 'https://gen.sendtric.com/countdown/6dnwe3yyrv',
+    NO: 'https://gen.sendtric.com/countdown/rb3380jl3l',
+    SK: 'https://gen.sendtric.com/countdown/46gpb5inz9',
+    BENL: 'https://gen.sendtric.com/countdown/fymqedlskp',
+    BEFR: 'https://gen.sendtric.com/countdown/7hy4idwbzy',
+    RO: 'https://gen.sendtric.com/countdown/75taulwyxl',
+  },
+};
+
+export default new entities.Campaign({
+  startId: '41453',
+  name: 'BBQ prep',
+  date: '05.03.2026',
+  issueCardId: '450612',
+  lpId: '29074',
+  alarm: {
+    isActive: false,
+  },
+  isArchive: false,
+  optimizeImg: false,
+  figmaUrl: 'https://www.figma.com/design/hfSpdlMDEh47Ht176jgbHr',
+  templates: [
+    {
+      name: 'Newsletter',
+      type: types.NEWSLETTER,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.NS,
+      translationsSpreadsheet: campaignTranslationsSheet,
+      background: '#FD9000',
+      color: '#000000',
+      wrapper: types.WRAPPER,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      timer: timer,
+      intro: {
+        color: '#000000', // unused
+        backgroundColor: '#FD9000', // unused
+        alignment: 'left',
+        type: 'paragraph',
+      },
+      Inside: {
+        color: '#000000',
+        backgroundColor: '#FFCCB7',
+        type: 'timer',
+      },
+    },
+    {
+      name: 'Landing',
+      type: types.LANDINGPAGE,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.LP,
+      background: '#FD9000',
+      color: '#000000',
+      translationsSpreadsheet: campaignTranslationsSheet,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      timer: timer,
+      intro: {
+        color: '#000000', // unused
+        backgroundColor: '#FD9000', // unused
+        alignment: 'left',
+        type: 'paragraph',
+      },
+      Inside: {
+        color: '#000000',
+        backgroundColor: '#FFCCB7',
+        type: 'timer',
+      },
+    },
+  ],
+});
