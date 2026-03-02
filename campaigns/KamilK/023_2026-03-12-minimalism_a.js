5bc735c4951adafffe788370e59e574bcd6de905
kkazzann <kamil.kazaniecki@beliani.net>
2026-03-02 15:02:06 +0100

    feat: add 12.03 camp

---
+++ campaigns/KamilK/023_2026-03-12-minimalism_a.js
@@ -0,0 +1,310 @@
+// Campaign generated from form
+const campaignTranslationsSheet = '2026::12.03.26 - Minimalism reshoot';
+
+const tableQueries = [
+  {
+    tableRange: '17:18',
+    name: 'TopImageTitle',
+  },
+  {
+    tableRange: '20',
+    name: 'intro',
+  },
+  {
+    tableRange: '21:24',
+    name: 'paragraphs',
+  },
+  {
+    tableRange: '25:26',
+    name: 'condition',
+  },
+  {
+    tableRange: '15:16',
+    name: 'timer',
+    tableName: '2026::Voucher - 09.03.26 - Free outdoor rugs	',
+  },
+];
+
+const links = {
+  TopImageTitle_href: translateLink({ value: 'content/lp26-03-12' }),
+  TopImageTitle_src: translateImage({ value: '20260312_01.png' }),
+
+  TopImage_src: getImageUrl('20260312_Pic.png', true),
+  TopImage_href: translateLink({ value: 'content/lp26-03-12' }),
+
+  Banner_1: translateLink({ value: 'content/lp26-03-05' }),
+  Banner_1_Image: translateImage({ value: '20260305b.png' }),
+
+  Banner_2: translateLink({ value: 'content/lp26-03-04' }),
+  Banner_2_Image: translateImage({ value: '20260304b.png' }),
+
+  Timer_href: translateLink({ value: 'content/lp26-03-09' }),
+};
+
+const TopImageTitle_data = {
+  color: '#000000',
+  backgroundColor: '#FFFCF9',
+  type: 'twoSameLines',
+};
+
+const categories = [
+  {
+    name: '',
+    src: getImageUrl('20260312_Cat00.png', true),
+    href: 'https://www.beliani.ch/trends/soft-minimalism-style/',
+    background: '#FAF8F7',
+    color: '#000000',
+    type: 'default',
+    cta: false,
+    paddingTop: '80',
+    spaceAfter: 0,
+    title: {
+      show: false,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'center',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+      align: 'center',
+    },
+    products: [
+      // VERNON
+      {
+        id: '430104',
+        src: getImageUrl('20260312_Pic01.png', true),
+      },
+    ],
+  },
+  {
+    // name: 'Garden chairs',
+    // src: getImageUrl('20260312_Cat10.png', true),
+    // href: 'https://www.beliani.ch/outdoor-furniture/garden-chairs/',
+    background: '#FAF8F7',
+    color: '#000000',
+    type: 'grid',
+    cta: false,
+    spaceAfter: 0,
+    gapBetweenHorizontal: false,
+    paddingTop: '0',
+    title: {
+      show: false,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      align: 'center',
+      spaceBefore: '0',
+      spaceAfter: 'newsletterBottom45px',
+    },
+    product: {
+      prices: true,
+      name: true,
+      align: 'center',
+    },
+    insideContainer: false,
+    products: [
+      // SAVEGRE
+      {
+        id: '689369',
+        src: getImageUrl('20260312_Pic02.png', true),
+      },
+      // ALCESTER
+      {
+        id: '570227',
+        src: getImageUrl('20260312_Pic03.png', true),
+      },
+      // HALLSBERG
+      {
+        id: '590599',
+        src: getImageUrl('20260312_Pic04.png', true),
+      },
+      // KENDALL
+      {
+        id: '581305',
+        src: getImageUrl('20260312_Pic05.png', true),
+      },
+    ],
+  },
+  {
+    name: '',
+    src: getImageUrl('20260312_Cat10.png', true),
+    href: 'https://www.beliani.ch/trends/soft-minimalism-style/',
+    background: '#FAF8F7',
+    color: '#000000',
+    type: 'default',
+    cta: false,
+    spaceAfter: 0,
+    paddingTop: '45',
+    title: {
+      show: false,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'center',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+      align: 'center',
+    },
+    products: [
+      // ASTORIA
+      {
+        id: '398859',
+        src: getImageUrl('20260312_Pic11.png', true),
+      },
+    ],
+  },
+  {
+    // name: 'Garden chairs',
+    // src: getImageUrl('20260312_Cat10.png', true),
+    href: 'https://www.beliani.ch/trends/soft-minimalism-style/',
+    background: '#FAF8F7',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    gapBetweenHorizontal: false,
+    paddingTop: '0',
+    title: {
+      show: false,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      align: 'center',
+      spaceBefore: '0',
+      spaceAfter: 'newsletterBottom45px',
+    },
+    product: {
+      prices: true,
+      name: true,
+      align: 'center',
+    },
+    insideContainer: false,
+    products: [
+      // DAGARI
+      {
+        id: '425617',
+        src: getImageUrl('20260312_Pic12.png', true),
+      },
+      // NORLEY
+      {
+        id: '227892',
+        src: getImageUrl('20260312_Pic13.png', true),
+      },
+      // GOBY
+      {
+        id: '552994',
+        src: getImageUrl('20260312_Pic14.png', true),
+      },
+      // ARENAS
+      {
+        id: '595957',
+        src: getImageUrl('20260312_Pic15.png', true),
+      },
+    ],
+  },
+];
+
+const timer = {
+  freebies: getImageUrl('20260312_Free.png', true),
+  image: {
+    CHDE: 'https://gen.sendtric.com/countdown/amlzix1nzx',
+    CHFR: 'https://gen.sendtric.com/countdown/jeq5zyo88u',
+    FR: 'https://gen.sendtric.com/countdown/h0i6v57kmg',
+    DE: 'https://gen.sendtric.com/countdown/9mi64zldcb',
+    UK: 'https://gen.sendtric.com/countdown/jr9rxzrr6q',
+    AT: 'https://gen.sendtric.com/countdown/0yr5zybehk',
+    ES: 'https://gen.sendtric.com/countdown/06arjazk6h',
+    PL: 'https://gen.sendtric.com/countdown/i8vlp3qfqr',
+    NL: 'https://gen.sendtric.com/countdown/jvrlujybqx',
+    PT: 'https://gen.sendtric.com/countdown/pqgylqvazv',
+    IT: 'https://gen.sendtric.com/countdown/vv8fa1z2jq',
+    SE: 'https://gen.sendtric.com/countdown/wrs8g0no10',
+    HU: 'https://gen.sendtric.com/countdown/h6pypgv8fj',
+    DK: 'https://gen.sendtric.com/countdown/sbrvdjhqfk',
+    CZ: 'https://gen.sendtric.com/countdown/jgokqfp97s',
+    FI: 'https://gen.sendtric.com/countdown/q1541jmh80',
+    NO: 'https://gen.sendtric.com/countdown/4vacrkbcev',
+    SK: 'https://gen.sendtric.com/countdown/4c8at0q1up',
+    BENL: 'https://gen.sendtric.com/countdown/tz1r996853',
+    BEFR: 'https://gen.sendtric.com/countdown/7b7cddo1a2',
+    RO: 'https://gen.sendtric.com/countdown/rlioil2qop',
+  },
+};
+
+export default new entities.Campaign({
+  startId: '41613',
+  name: 'Minimalism [GROUP A]',
+  date: '12.03.2026',
+  issueCardId: '455215',
+  lpId: '29152',
+  alarm: {
+    isActive: false,
+  },
+  isArchive: false,
+  optimizeImg: false,
+  figmaUrl: 'https://www.figma.com/design/so0OgpBMXfoAGDDxIdOUD2',
+  templates: [
+    {
+      name: 'Newsletter',
+      type: types.NEWSLETTER,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.NS,
+      translationsSpreadsheet: campaignTranslationsSheet,
+      background: '#FAF8F7',
+      color: '#000000',
+      wrapper: types.WRAPPER,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      timer: timer,
+      intro: {
+        color: '#000000', // unused
+        backgroundColor: '#FAF8F7', // unused
+        alignment: 'center',
+        type: 'paragraph',
+      },
+      Inside: {
+        color: '#ffffff',
+        backgroundColor: '#750000',
+        type: 'timer',
+      },
+    },
+    {
+      name: 'Landing',
+      type: types.LANDINGPAGE,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.LP,
+      background: '#FAF8F7',
+      color: '#000000',
+      translationsSpreadsheet: campaignTranslationsSheet,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      timer: timer,
+      intro: {
+        color: '#000000', // unused
+        backgroundColor: '#FAF8F7', // unused
+        alignment: 'center',
+        type: 'paragraph',
+      },
+      Inside: {
+        color: '#ffffff',
+        backgroundColor: '#750000',
+        type: 'timer',
+      },
+    },
+  ],
+});
---
+++ campaigns/KamilK/023_2026-03-12-minimalism_b.js
@@ -0,0 +1,310 @@
+// Campaign generated from form
+const campaignTranslationsSheet = '2026::12.03.26 - Minimalism reshoot';
+
+const tableQueries = [
+  {
+    tableRange: '17:18',
+    name: 'TopImageTitle',
+  },
+  {
+    tableRange: '20',
+    name: 'intro',
+  },
+  {
+    tableRange: '21:24',
+    name: 'paragraphs',
+  },
+  {
+    tableRange: '25:26',
+    name: 'condition',
+  },
+  {
+    tableRange: '15:16',
+    name: 'timer',
+    tableName: '2026::Voucher - 09.03.26 - Free outdoor rugs	',
+  },
+];
+
+const links = {
+  TopImageTitle_href: translateLink({ value: 'content/lp26-03-12' }),
+  TopImageTitle_src: translateImage({ value: '20260312_01.png' }),
+
+  TopImage_src: getImageUrl('20260312_Pic.png', true),
+  TopImage_href: translateLink({ value: 'content/lp26-03-12' }),
+
+  Banner_1: translateLink({ value: 'content/lp26-03-05' }),
+  Banner_1_Image: translateImage({ value: '20260305b.png' }),
+
+  Banner_2: translateLink({ value: 'content/lp26-03-04' }),
+  Banner_2_Image: translateImage({ value: '20260304b.png' }),
+
+  Timer_href: translateLink({ value: 'content/lp26-03-09' }),
+};
+
+const TopImageTitle_data = {
+  color: '#000000',
+  backgroundColor: '#FFFCF9',
+  type: 'twoSameLines',
+};
+
+const categories = [
+  {
+    name: '',
+    src: getImageUrl('20260312_Cat00.png', true),
+    href: 'https://www.beliani.ch/trends/soft-minimalism-style/',
+    background: '#FAF8F7',
+    color: '#000000',
+    type: 'default',
+    cta: false,
+    paddingTop: '80',
+    spaceAfter: 0,
+    title: {
+      show: false,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'center',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: false,
+      name: true,
+      align: 'center',
+    },
+    products: [
+      // VERNON
+      {
+        id: '430104',
+        src: getImageUrl('20260312_Pic01.png', true),
+      },
+    ],
+  },
+  {
+    // name: 'Garden chairs',
+    // src: getImageUrl('20260312_Cat10.png', true),
+    // href: 'https://www.beliani.ch/outdoor-furniture/garden-chairs/',
+    background: '#FAF8F7',
+    color: '#000000',
+    type: 'grid',
+    cta: false,
+    spaceAfter: 0,
+    gapBetweenHorizontal: false,
+    paddingTop: '0',
+    title: {
+      show: false,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      align: 'center',
+      spaceBefore: '0',
+      spaceAfter: 'newsletterBottom45px',
+    },
+    product: {
+      prices: false,
+      name: true,
+      align: 'center',
+    },
+    insideContainer: false,
+    products: [
+      // SAVEGRE
+      {
+        id: '689369',
+        src: getImageUrl('20260312_Pic02.png', true),
+      },
+      // ALCESTER
+      {
+        id: '570227',
+        src: getImageUrl('20260312_Pic03.png', true),
+      },
+      // HALLSBERG
+      {
+        id: '590599',
+        src: getImageUrl('20260312_Pic04.png', true),
+      },
+      // KENDALL
+      {
+        id: '581305',
+        src: getImageUrl('20260312_Pic05.png', true),
+      },
+    ],
+  },
+  {
+    name: '',
+    src: getImageUrl('20260312_Cat10.png', true),
+    href: 'https://www.beliani.ch/trends/soft-minimalism-style/',
+    background: '#FAF8F7',
+    color: '#000000',
+    type: 'default',
+    cta: false,
+    spaceAfter: 0,
+    paddingTop: '45',
+    title: {
+      show: false,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'center',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: false,
+      name: true,
+      align: 'center',
+    },
+    products: [
+      // ASTORIA
+      {
+        id: '398859',
+        src: getImageUrl('20260312_Pic11.png', true),
+      },
+    ],
+  },
+  {
+    // name: 'Garden chairs',
+    // src: getImageUrl('20260312_Cat10.png', true),
+    href: 'https://www.beliani.ch/trends/soft-minimalism-style/',
+    background: '#FAF8F7',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    gapBetweenHorizontal: false,
+    paddingTop: '0',
+    title: {
+      show: false,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      align: 'center',
+      spaceBefore: '0',
+      spaceAfter: 'newsletterBottom45px',
+    },
+    product: {
+      prices: false,
+      name: true,
+      align: 'center',
+    },
+    insideContainer: false,
+    products: [
+      // DAGARI
+      {
+        id: '425617',
+        src: getImageUrl('20260312_Pic12.png', true),
+      },
+      // NORLEY
+      {
+        id: '227892',
+        src: getImageUrl('20260312_Pic13.png', true),
+      },
+      // GOBY
+      {
+        id: '552994',
+        src: getImageUrl('20260312_Pic14.png', true),
+      },
+      // ARENAS
+      {
+        id: '595957',
+        src: getImageUrl('20260312_Pic15.png', true),
+      },
+    ],
+  },
+];
+
+const timer = {
+  freebies: getImageUrl('20260312_Free.png', true),
+  image: {
+    CHDE: 'https://gen.sendtric.com/countdown/c9k9cewr5k',
+    CHFR: 'https://gen.sendtric.com/countdown/dlsqxbjdwn',
+    FR: 'https://gen.sendtric.com/countdown/xd041jrodb',
+    DE: 'https://gen.sendtric.com/countdown/qzkmh50sr7',
+    UK: 'https://gen.sendtric.com/countdown/uq35m77oed',
+    AT: 'https://gen.sendtric.com/countdown/4k95f81om4',
+    ES: 'https://gen.sendtric.com/countdown/h2vf8f9vcg',
+    PL: 'https://gen.sendtric.com/countdown/59oncqnq28',
+    NL: 'https://gen.sendtric.com/countdown/x6qsrr47wd',
+    PT: 'https://gen.sendtric.com/countdown/u9yxddppr4',
+    IT: 'https://gen.sendtric.com/countdown/yt2xasq0hj',
+    SE: 'https://gen.sendtric.com/countdown/gx3ra266xl',
+    HU: 'https://gen.sendtric.com/countdown/b91pia1les',
+    DK: 'https://gen.sendtric.com/countdown/0mgig4mm9k',
+    CZ: 'https://gen.sendtric.com/countdown/6487va2mlo',
+    FI: 'https://gen.sendtric.com/countdown/o8t3p5a2jn',
+    NO: 'https://gen.sendtric.com/countdown/8ag1hbuj4c',
+    SK: 'https://gen.sendtric.com/countdown/76dak6jl70',
+    BENL: 'https://gen.sendtric.com/countdown/bxgh0grekm',
+    BEFR: 'https://gen.sendtric.com/countdown/axavtb5y50',
+    RO: 'https://gen.sendtric.com/countdown/vc4xf0jg6k',
+  },
+};
+
+export default new entities.Campaign({
+  startId: '41901',
+  name: 'Minimalism [GROUP B]',
+  date: '12.03.2026',
+  issueCardId: '455215',
+  lpId: '',
+  alarm: {
+    isActive: false,
+  },
+  isArchive: false,
+  optimizeImg: false,
+  figmaUrl: 'https://www.figma.com/design/so0OgpBMXfoAGDDxIdOUD2',
+  templates: [
+    {
+      name: 'Newsletter',
+      type: types.NEWSLETTER,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.NS,
+      translationsSpreadsheet: campaignTranslationsSheet,
+      background: '#FAF8F7',
+      color: '#000000',
+      wrapper: types.WRAPPER,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      timer: timer,
+      intro: {
+        color: '#000000', // unused
+        backgroundColor: '#FAF8F7', // unused
+        alignment: 'center',
+        type: 'paragraph',
+      },
+      Inside: {
+        color: '#ffffff',
+        backgroundColor: '#750000',
+        type: 'timer',
+      },
+    },
+    {
+      name: 'Landing',
+      type: types.LANDINGPAGE,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.LP,
+      background: '#FAF8F7',
+      color: '#000000',
+      translationsSpreadsheet: campaignTranslationsSheet,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      timer: timer,
+      intro: {
+        color: '#000000', // unused
+        backgroundColor: '#FAF8F7', // unused
+        alignment: 'center',
+        type: 'paragraph',
+      },
+      Inside: {
+        color: '#ffffff',
+        backgroundColor: '#750000',
+        type: 'timer',
+      },
+    },
+  ],
+});
