48603cc1f53cee5e178f8c12a9bf7a2593dc106a
kkazzann <kamil.kazaniecki@beliani.net>
2026-03-04 10:20:53 +0100

    feat: add 19.03 camp

---
+++ campaigns/KamilK/024_2026-03-19-outdoor-new-arrivals.js
@@ -0,0 +1,365 @@
+// Campaign generated from form
+const campaignTranslationsSheet = '2026::19.03.26 - Outdoor new arrivals';
+
+const tableQueries = [
+  {
+    tableRange: '17:18',
+    name: 'TopImageTitle',
+  },
+  {
+    tableRange: '20',
+    name: 'intro',
+  },
+  {
+    tableRange: '21',
+    name: 'paragraphs',
+  },
+  {
+    tableRange: '22:24',
+    name: 'categoryLinks',
+  },
+  {
+    tableRange: '25:26',
+    name: 'condition',
+  },
+  {
+    tableRange: '15:16',
+    name: 'timer',
+    tableName: '2026::16.03.2026 - Free Gift',
+  },
+];
+
+const links = {
+  TopImageTitle_href: translateLink({ value: 'content/lp26-03-19' }),
+  TopImageTitle_src: translateImage({ value: '20260319_01.png' }),
+
+  TopImage_src: getImageUrl('20260319_Pic.png', true),
+  TopImage_href: translateLink({ value: 'content/lp26-03-19' }),
+
+  Banner_1: translateLink({ value: 'content/lp26-03-12' }),
+  Banner_1_Image: translateImage({ value: '20260312b.png' }),
+
+  Banner_2: translateLink({ value: 'content/lp26-03-11' }),
+  Banner_2_Image: translateImage({ value: '20260311b.png' }),
+
+  Timer_href: translateLink({ value: 'content/lp26-03-16' }),
+};
+
+const TopImageTitle_data = {
+  color: '#000000',
+  backgroundColor: '#FECD8C',
+  type: 'twoSameLines',
+};
+
+const categories = [
+  {
+    // name: 'Dining room',
+    src: getImageUrl('20260319_Cat00.png', true),
+    // href: 'https://www.beliani.ch/dining-room-furniture/',
+    background: '#FECD8C',
+    color: '#000000',
+    type: '1Big-2Vertical',
+    cta: true,
+    paddingTop: '60',
+    title: {
+      show: false,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: false,
+      name: false,
+    },
+    products: [
+      // OMEGANA - big img left
+      {
+        id: '695839',
+        src: getImageUrl('20260319_Cat01.png', true),
+        useCategoryLink: true,
+        // spaceAfter: '0',
+      },
+      // OMEGANA
+      {
+        id: '695839',
+        src: translateImage({ value: '20260319_Pic01.png' }),
+        // spaceAfter: '0',
+      },
+      // LEFFE
+      {
+        id: '702511',
+        src: translateImage({ value: '20260319_Pic02.png' }),
+        // spaceAfter: '0',
+      },
+
+      // subcategory ....................................
+
+      // MUROS - big img right
+      {
+        id: '	707128',
+        src: getImageUrl('20260319_Cat02.png', true),
+        useCategoryLink: true,
+        // spaceAfter: '0',
+      },
+
+      // MUROS
+      {
+        id: '	707128',
+        src: translateImage({ value: '20260319_Pic03.png' }),
+        // spaceAfter: '0',
+      },
+
+      // SUNO
+      {
+        id: '701115',
+        src: translateImage({ value: '20260319_Pic04.png' }),
+        // spaceAfter: '0',
+      },
+    ],
+  },
+
+  {
+    // name: 'Dining room',
+    src: getImageUrl('20260319_Cat10.png', true),
+    // href: 'https://www.beliani.ch/dining-room-furniture/',
+    background: '#FECD8C',
+    color: '#000000',
+    type: '1Big-2Vertical',
+    cta: true,
+    paddingTop: '0',
+    imageSide: 'left',
+    title: {
+      show: false,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: false,
+      name: false,
+    },
+    products: [
+      // SULMONA - big img left
+      {
+        id: '704482',
+        src: getImageUrl('20260319_Cat11.png', true),
+        useCategoryLink: true,
+        // spaceAfter: '0',
+      },
+      // SULMONA
+      {
+        id: '704482',
+        src: translateImage({ value: '20260319_Pic11.png' }),
+        // spaceAfter: '0',
+      },
+      // MARCETELLI
+      {
+        id: '687805',
+        src: translateImage({ value: '20260319_Pic12.png' }),
+        // spaceAfter: '0',
+      },
+
+      // subcategory ....................................
+
+      // ZERVATA - big img right
+      {
+        id: '684414',
+        src: getImageUrl('20260319_Cat12.png', true),
+        useCategoryLink: true,
+        // spaceAfter: '0',
+      },
+
+      // ZERVATA
+      {
+        id: '684414',
+        src: translateImage({ value: '20260319_Pic13.png' }),
+        // spaceAfter: '0',
+      },
+
+      // BAIYUN
+      {
+        id: '706729',
+        src: translateImage({ value: '20260319_Pic14.png' }),
+        // spaceAfter: '0',
+      },
+    ],
+  },
+
+  {
+    // name: 'Dining room',
+    src: getImageUrl('20260319_Cat20.png', true),
+    // href: 'https://www.beliani.ch/dining-room-furniture/',
+    background: '#FECD8C',
+    color: '#000000',
+    type: '1Big-2Vertical',
+    cta: true,
+    paddingTop: '0',
+    title: {
+      show: false,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: false,
+      name: false,
+    },
+    products: [
+      // XITTA - big img left
+      {
+        id: '	611684',
+        src: getImageUrl('20260319_Cat21.png', true),
+        useCategoryLink: true,
+        // spaceAfter: '0',
+      },
+      // MAENZA
+      {
+        id: '701479',
+        src: translateImage({ value: '20260319_Pic21.png' }),
+        // spaceAfter: '0',
+      },
+      // XITTA
+      {
+        id: '	611684',
+        src: translateImage({ value: '20260319_Pic22.png' }),
+        // spaceAfter: '0',
+      },
+
+      // subcategory ....................................
+
+      // PRINOS - big img right
+      {
+        id: '660812',
+        src: getImageUrl('20260319_Cat22.png', true),
+        useCategoryLink: true,
+        // spaceAfter: '0',
+      },
+
+      // PRINOS
+      {
+        id: '660812',
+        src: translateImage({ value: '20260319_Pic23.png' }),
+        // spaceAfter: '0',
+      },
+
+      // MILIES
+      {
+        id: '	660699',
+        src: translateImage({ value: '20260319_Pic24.png' }),
+        // spaceAfter: '0',
+      },
+    ],
+  },
+];
+
+const timer = {
+  freebies: getImageUrl('20260319_Free.png', true),
+  image: {
+    CHDE: 'https://gen.sendtric.com/countdown/gfvwy5vjj1',
+    CHFR: 'https://gen.sendtric.com/countdown/zjlnud4zad',
+    FR: 'https://gen.sendtric.com/countdown/3yvaazm6gw',
+    DE: 'https://gen.sendtric.com/countdown/mcgb10x3lm',
+    UK: 'https://gen.sendtric.com/countdown/yibart2xx1',
+    AT: 'https://gen.sendtric.com/countdown/ljb5jk5hks',
+    ES: 'https://gen.sendtric.com/countdown/ksqld5jpwj',
+    PL: 'https://gen.sendtric.com/countdown/r5d62v9qb8',
+    NL: 'https://gen.sendtric.com/countdown/m5axoo2d33',
+    PT: 'https://gen.sendtric.com/countdown/6sdiys14sg',
+    IT: 'https://gen.sendtric.com/countdown/1zmhbgxiqt',
+    SE: 'https://gen.sendtric.com/countdown/gwf2vg71l8',
+    HU: 'https://gen.sendtric.com/countdown/qt1r7trg7u',
+    DK: 'https://gen.sendtric.com/countdown/pnte8osmq9',
+    CZ: 'https://gen.sendtric.com/countdown/4exrxd8via',
+    FI: 'https://gen.sendtric.com/countdown/2db8tzzosw',
+    NO: 'https://gen.sendtric.com/countdown/vgah7mf9hy',
+    SK: 'https://gen.sendtric.com/countdown/0vt1w09jx7',
+    BENL: 'https://gen.sendtric.com/countdown/3rvc02tymo',
+    BEFR: 'https://gen.sendtric.com/countdown/sdnqmw4n9c',
+    RO: 'https://gen.sendtric.com/countdown/08c4nq32kk',
+  },
+};
+
+export default new entities.Campaign({
+  startId: '42029',
+  name: 'Outdoor new arrivals',
+  date: '19.03.2026',
+  issueCardId: '457547',
+  lpId: '29304',
+  alarm: {
+    isActive: false,
+  },
+  isArchive: false,
+  optimizeImg: false,
+  figmaUrl: 'https://www.figma.com/design/ln0pg3MFjZE4zMLrJFXf53/',
+  templates: [
+    {
+      name: 'Newsletter',
+      type: types.NEWSLETTER,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.NS,
+      translationsSpreadsheet: campaignTranslationsSheet,
+      background: '#FECD8C',
+      color: '#000000',
+      wrapper: types.WRAPPER,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      timer: timer,
+      intro: {
+        cta: {
+          spaceAfter: 'newsletterBottom80px',
+        },
+        color: '#000000', // unused
+        backgroundColor: '#FECD8C', // unused
+        alignment: 'left',
+        type: 'paragraph',
+      },
+      Inside: {
+        color: '#FFFFFF',
+        backgroundColor: '#750000',
+        type: 'timer',
+      },
+    },
+    {
+      name: 'Landing',
+      type: types.LANDINGPAGE,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.LP,
+      background: '#FECD8C',
+      color: '#000000',
+      translationsSpreadsheet: campaignTranslationsSheet,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      timer: timer,
+      intro: {
+        cta: {
+          spaceAfter: 'newsletterBottom80px',
+        },
+        color: '#000000', // unused
+        backgroundColor: '#FECD8C', // unused
+        alignment: 'left',
+        type: 'paragraph',
+      },
+      Inside: {
+        color: '#FFFFFF',
+        backgroundColor: '#750000',
+        type: 'timer',
+      },
+    },
+  ],
+});
