719bc9ee2d09ad08cdbcbba5fd107ea14c70eeb3
kkazzann <kamil.kazaniecki@beliani.net>
2026-03-16 10:12:16 +0100

    feat: add 26.03 camp (a/b)

---
+++ campaigns/KamilK/027_2026-03-26-industrial_GROUP_A.js
@@ -0,0 +1,293 @@
+// Campaign generated from form
+const campaignTranslationsSheet = '2026::26.03.26 - Industrial';
+
+const tableQueries = [
+  {
+    tableRange: '17:18',
+    name: 'TopImageTitle',
+  },
+  {
+    tableRange: '20',
+    name: 'intro',
+  },
+  {
+    tableRange: '21:22',
+    name: 'paragraphs',
+  },
+  {
+    // change for group b to 25:26
+    tableRange: '23:24',
+    name: 'condition',
+  },
+  {
+    tableRange: '15:16',
+    name: 'timer',
+    tableName: '2026::Voucher - 24.03.26 - Free flower pot',
+  },
+];
+
+const links = {
+  TopImageTitle_href: translateLink({ value: 'content/lp26-03-26' }),
+  TopImageTitle_src: translateImage({ value: '20260326_01.png' }),
+
+  TopImage_src: getImageUrl('20260326_Gif.gif', true),
+  TopImage_href: translateLink({ value: 'content/lp26-03-26' }),
+
+  Banner_1: translateLink({ value: 'content/lp26-03-19' }),
+  Banner_1_Image: translateImage({ value: '20260319b.png' }),
+
+  Banner_2: translateLink({ value: 'content/lp26-03-18' }),
+  Banner_2_Image: translateImage({ value: '20260318b.png' }),
+
+  Timer_href: translateLink({ value: 'content/lp26-03-24' }),
+};
+
+const TopImageTitle_data = {
+  color: '#000000',
+  backgroundColor: '#FFDCCD',
+  type: 'standard_inverted',
+};
+
+const categories = [
+  {
+    // name: 'Garden chairs',
+    src: getImageUrl('20260326_Cat00.png', true),
+    href: 'https://www.beliani.ch/meet-beliani/meet-beliani-industrial-style/',
+    background: '#FFDCCD',
+    color: '#000000',
+    type: '3prods',
+    cta: false,
+    spaceAfter: 0,
+    spaceBeforeProducts: 'newsletterBottom35px',
+    tdClass: "newsletterContainer",
+    gapBetweenHorizontal: true,
+    paddingTop: '60',
+    title: {
+      show: false,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      position: 'afterProducts',
+      align: 'left',
+      spaceBefore: 'newsletterBottom45px',
+      spaceAfter: '0',
+    },
+    product: {
+      prices: true,
+      name: true,
+      align: 'center',
+    },
+    // insideContainer: false,
+    products: [
+      // MANDAI
+      {
+        id: '423505',
+        src: getImageUrl('20260326_Pic01.png', true),
+      },
+      // LUSPA
+      {
+        id: '	449303',
+        src: getImageUrl('20260326_Pic02.png', true),
+      },
+      // ALTOS
+      {
+        id: '97777',
+        src: getImageUrl('20260326_Pic03.png', true),
+      },
+    ],
+  },
+  {
+    name: '',
+    src: getImageUrl('20260326_Cat10.png', true),
+    href: 'https://www.beliani.ch/meet-beliani/meet-beliani-industrial-style/',
+    background: '#FFDCCD',
+    color: '#000000',
+    type: '3prods',
+    cta: false,
+    spaceAfter: 0,
+    tdClass: "newsletterContainer",
+    spaceBeforeProducts: 'newsletterBottom35px',
+    paddingTop: '35',
+    title: {
+      show: false,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      position: 'afterProducts',
+      align: 'left',
+      spaceBefore: 'newsletterBottom45px',
+      spaceAfter: '0',
+    },
+    product: {
+      prices: true,
+      name: true,
+      align: 'center',
+    },
+    products: [
+      // STACY
+      {
+        id: '	568950',
+        src: getImageUrl('20260326_Pic11.png', true),
+      },
+      // BRONSON
+      {
+        id: '236134',
+        src: getImageUrl('20260326_Pic12.png', true),
+      },
+      // LAMEGO
+      {
+        id: '600562',
+        src: getImageUrl('20260326_Pic13.png', true),
+      },
+    ],
+  },
+  {
+    // name: 'Garden chairs',
+    src: getImageUrl('20260326_Cat20.png', true),
+    href: 'https://www.beliani.ch/meet-beliani/meet-beliani-industrial-style/',
+    background: '#FFDCCD',
+    color: '#000000',
+    type: '3prods',
+    cta: true,
+    tdClass: "newsletterContainer",
+    spaceBeforeProducts: 'newsletterBottom35px',
+    gapBetweenHorizontal: true,
+    paddingTop: '35',
+    title: {
+      show: false,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      position: 'afterProducts',
+      align: 'center',
+      spaceBefore: '0',
+      spaceAfter: '0',
+    },
+    product: {
+      prices: true,
+      name: true,
+      align: 'center',
+    },
+    // insideContainer: true,
+    products: [
+      // STILETTO
+      {
+        id: '49788',
+        src: getImageUrl('20260326_Pic21.png', true),
+      },
+      // CHIVES
+      {
+        id: '684185',
+        src: getImageUrl('20260326_Pic22.png', true),
+      },
+      // PEREA
+      {
+        id: '363701',
+        src: getImageUrl('20260326_Pic23.png', true),
+      },
+    ],
+  },
+];
+
+const timer = {
+  freebies: getImageUrl('20260326_Free.png', true),
+  image: {
+    CHDE: 'https://gen.sendtric.com/countdown/o38jc8d7ge',
+    CHFR: 'https://gen.sendtric.com/countdown/svb4mtmf0e',
+    FR: 'https://gen.sendtric.com/countdown/9mix2t3tgv',
+    DE: 'https://gen.sendtric.com/countdown/rzy4vi89kv',
+    UK: 'https://gen.sendtric.com/countdown/5h566ex0bd',
+    AT: 'https://gen.sendtric.com/countdown/96llsr2sq1',
+    ES: 'https://gen.sendtric.com/countdown/rkw8e8lqyt',
+    PL: 'https://gen.sendtric.com/countdown/cw2vdbv17e',
+    NL: 'https://gen.sendtric.com/countdown/75hwg7gikx',
+    PT: 'https://gen.sendtric.com/countdown/2nvq04m1kt',
+    IT: 'https://gen.sendtric.com/countdown/rze3z9a3b8',
+    SE: 'https://gen.sendtric.com/countdown/heri74asmw',
+    HU: 'https://gen.sendtric.com/countdown/nwz8xei0xm',
+    DK: 'https://gen.sendtric.com/countdown/0ksg8kqf58',
+    CZ: 'https://gen.sendtric.com/countdown/wqt9jpendd',
+    FI: 'https://gen.sendtric.com/countdown/wbawoet43p',
+    NO: 'https://gen.sendtric.com/countdown/seh6tvam4x',
+    SK: 'https://gen.sendtric.com/countdown/foya3pi7ke',
+    BENL: 'https://gen.sendtric.com/countdown/hlr80xf9ku',
+    BEFR: 'https://gen.sendtric.com/countdown/66lg5tv4zw',
+    RO: 'https://gen.sendtric.com/countdown/wlamtn4rvw',
+  },
+};
+
+export default new entities.Campaign({
+  startId: '42253',
+  name: 'Industrial [GROUP A]',
+  date: '26.03.2026',
+  issueCardId: '459335',
+  lpId: '29420',
+  alarm: {
+    isActive: false,
+  },
+  isArchive: false,
+  optimizeImg: false,
+  figmaUrl: 'https://www.figma.com/design/PzBB9cFdnofljKqoK94rzs/',
+  templates: [
+    {
+      name: 'Newsletter',
+      type: types.NEWSLETTER,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.NS,
+      translationsSpreadsheet: campaignTranslationsSheet,
+      background: '#FFDCCD',
+      color: '#000000',
+      wrapper: types.WRAPPER,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      timer: timer,
+      intro: {
+        color: '#000000', // unused
+        backgroundColor: '#FFDCCD', // unused
+        alignment: 'left',
+        type: 'paragraph',
+        cta: {
+          spaceAfter: 'newsletterBottom35px',
+        },
+      },
+      Inside: {
+        color: '#000000',
+        backgroundColor: '#F6E7E6',
+        type: 'timer',
+      },
+    },
+    {
+      name: 'Landing',
+      type: types.LANDINGPAGE,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.LP,
+      background: '#FFDCCD',
+      color: '#000000',
+      translationsSpreadsheet: campaignTranslationsSheet,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      timer: timer,
+      intro: {
+        color: '#000000', // unused
+        backgroundColor: '#FFDCCD', // unused
+        alignment: 'left',
+        type: 'paragraph',
+        cta: {
+          spaceAfter: 'newsletterBottom35px',
+        },
+      },
+      Inside: {
+        color: '#000000',
+        backgroundColor: '#F6E7E6',
+        type: 'timer',
+      },
+    },
+  ],
+});
