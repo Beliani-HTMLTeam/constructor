c096bb36afcdf4f899cdcf2b3ae86cd3f32278d7
kkazzann <kamil.kazaniecki@beliani.net>
2026-03-20 12:02:43 +0100

    feat: add 02.04 camp

---
+++ campaigns/KamilK/030_2026-04-02-hallway.js
@@ -0,0 +1,333 @@
+// Campaign generated from form
+const campaignTranslationsSheet = '2026::02.04.26 - Hallway';
+
+const tableQueries = [
+  {
+    tableRange: '17:18',
+    name: 'TopImageTitle',
+  },
+  {
+    tableRange: '20',
+    name: 'intro',
+  },
+  {
+    tableRange: '25:28',
+    name: 'paragraphs',
+  },
+  {
+    tableRange: '21:24',
+    name: 'categories',
+  },
+  {
+    tableRange: '29:30',
+    name: 'condition',
+  },
+  {
+    tableRange: '15:16',
+    name: 'timer',
+    tableName: '2026::Voucher - 27.03.26 - Cashback',
+  },
+];
+
+const links = {
+  TopImageTitle_href: translateLink({ value: 'content/lp26-04-02' }),
+  TopImageTitle_src: translateImage({ value: '20260402_01.png' }),
+
+  TopImage_src: getImageUrl('20260402_Gif.gif', true),
+  TopImage_href: translateLink({ value: 'content/lp26-04-02' }),
+
+  Banner_1: translateLink({ value: 'content/lp26-03-26' }),
+  Banner_1_Image: translateImage({ value: '20260326b.png' }),
+
+  Banner_2: translateLink({ value: 'content/lp26-03-25' }),
+  Banner_2_Image: translateImage({ value: '20260325b.png' }),
+
+  Timer_href: translateLink({ value: 'content/lp26-03-27' }),
+};
+
+const TopImageTitle_data = {
+  color: '#000000',
+  backgroundColor: '#FFF4E6',
+  type: 'twoSameLines',
+};
+
+const categories = [
+  {
+    name: 'Console tables',
+    src: getImageUrl('20260402_Cat00.png', true),
+    href: 'https://www.beliani.ch/tables/console-tables/',
+    background: '#FFF4E6',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '60',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // BONDI
+      {
+        id: '682746',
+        src: getImageUrl('20260402_Pic01.png', true),
+      },
+      // KENDALL
+      {
+        id: '707147',
+        src: getImageUrl('20260402_Pic02.png', true),
+      },
+      // BIRSON
+      {
+        id: '642994',
+        src: getImageUrl('20260402_Pic03.png', true),
+      },
+      // LOVELL
+      {
+        id: '556635',
+        src: getImageUrl('20260402_Pic04.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Benches',
+    src: getImageUrl('20260402_Cat10.png', true),
+    href: 'https://www.beliani.ch/hallway-seating/hallway-benches/',
+    background: '#FD9000',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '35',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // SOLVANG
+      {
+        id: '699401',
+        src: getImageUrl('20260402_Pic11.png', true),
+      },
+      // DECORAH
+      {
+        id: '702586',
+        src: getImageUrl('20260402_Pic12.png', true),
+      },
+      // LEWES
+      {
+        id: '699131',
+        src: getImageUrl('20260402_Pic13.png', true),
+      },
+      // MILRY
+      {
+        id: '697637',
+        src: getImageUrl('20260402_Pic14.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Shoe cabinets',
+    src: getImageUrl('20260402_Cat20.png', true),
+    href: 'https://www.beliani.ch/storage/shoe-cabinets/',
+    background: '#FFF4E6',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '35',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // BUCHANAN
+      {
+        id: '708180',
+        src: getImageUrl('20260402_Pic21.png', true),
+      },
+      // PENTICTON
+      {
+        id: '682501',
+        src: getImageUrl('20260402_Pic22.png', true),
+      },
+      // MOONIE
+      {
+        id: '662129',
+        src: getImageUrl('20260402_Pic23.png', true),
+      },
+      // MOURA
+      {
+        id: '662319',
+        src: getImageUrl('20260402_Pic24.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Coat stands & hangers',
+    src: getImageUrl('20260402_Cat30.png', true),
+    href: 'https://www.beliani.ch/storage/coat-stands/',
+    background: '#FD9000',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '35',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // NOTUS
+      {
+        id: '698897',
+        src: getImageUrl('20260402_Pic31.png', true),
+      },
+      // LAWTON
+      {
+        id: '	559628',
+        src: getImageUrl('20260402_Pic32.png', true),
+      },
+      // EUREKA
+      {
+        id: '553275',
+        src: getImageUrl('20260402_Pic33.png', true),
+      },
+      // ORDWAY
+      {
+        id: '559799',
+        src: getImageUrl('20260402_Pic34.png', true),
+      },
+    ],
+  },
+];
+
+const timer = {
+  // freebies: getImageUrl('20260305free.png', true),
+  image: {
+    CHDE: 'https://gen.sendtric.com/countdown/x4xkaoo833',
+    CHFR: 'https://gen.sendtric.com/countdown/8cm8my2nvg',
+    FR: 'https://gen.sendtric.com/countdown/trgleoqtpy',
+    DE: 'https://gen.sendtric.com/countdown/okxijj65l8',
+    UK: 'https://gen.sendtric.com/countdown/zm3lhef1xg',
+    AT: 'https://gen.sendtric.com/countdown/1lo6wc5796',
+    ES: 'https://gen.sendtric.com/countdown/344ol9rz3g',
+    PL: 'https://gen.sendtric.com/countdown/0jduyfnn4m',
+    NL: 'https://gen.sendtric.com/countdown/hx4o6sk7bt',
+    PT: 'https://gen.sendtric.com/countdown/3iwloy5voj',
+    IT: 'https://gen.sendtric.com/countdown/ceq2eu24rj',
+    SE: 'https://gen.sendtric.com/countdown/anymyxcdq9',
+    HU: 'https://gen.sendtric.com/countdown/7zibr98hg5',
+    DK: 'https://gen.sendtric.com/countdown/pb4qam5r0t',
+    CZ: 'https://gen.sendtric.com/countdown/h4g7ddf95q',
+    FI: 'https://gen.sendtric.com/countdown/pyp354sbdi',
+    NO: 'https://gen.sendtric.com/countdown/anry4ro5or',
+    SK: 'https://gen.sendtric.com/countdown/k2krqe4x01',
+    BENL: 'https://gen.sendtric.com/countdown/0mjkuqw220',
+    BEFR: 'https://gen.sendtric.com/countdown/uvizlekq19',
+    RO: 'https://gen.sendtric.com/countdown/xn13t5vwy9',
+  },
+};
+
+export default new entities.Campaign({
+  startId: '42509',
+  name: 'Hallway',
+  date: '02.04.2026',
+  issueCardId: '459872',
+  lpId: '29534',
+  alarm: {
+    isActive: false,
+  },
+  isArchive: false,
+  optimizeImg: false,
+  figmaUrl: 'https://www.figma.com/design/4cNPVcYyJJ9PqEHqsMPXLw/',
+  templates: [
+    {
+      name: 'Newsletter',
+      type: types.NEWSLETTER,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.NS,
+      translationsSpreadsheet: campaignTranslationsSheet,
+      background: '#FFF4E6',
+      color: '#000000',
+      wrapper: types.WRAPPER,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      timer: timer,
+      intro: {
+        color: '#000000', // unused
+        backgroundColor: '#FFF4E6', // unused
+        alignment: 'left',
+        type: 'paragraph',
+      },
+      Inside: {
+        color: '#ffffff',
+        backgroundColor: '#FF2F00',
+        type: 'timer',
+      },
+    },
+    {
+      name: 'Landing',
+      type: types.LANDINGPAGE,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.LP,
+      background: '#FFF4E6',
+      color: '#000000',
+      translationsSpreadsheet: campaignTranslationsSheet,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      timer: timer,
+      intro: {
+        color: '#000000', // unused
+        backgroundColor: '#FFF4E6', // unused
+        alignment: 'left',
+        type: 'paragraph',
+      },
+      Inside: {
+        color: '#ffffff',
+        backgroundColor: '#FF2F00',
+        type: 'timer',
+      },
+    },
+  ],
+});
