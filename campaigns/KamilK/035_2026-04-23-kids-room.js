80b79f5b5dcf427d7f6c1e50d4a1fd5f942e64ce
kkazzann <kamil.kazaniecki@beliani.net>
2026-04-08 11:46:41 +0200

    feat: add 23.04 camp

---
+++ campaigns/KamilK/035_2026-04-23-kids-room.js
@@ -0,0 +1,333 @@
+// Campaign generated from form
+const campaignTranslationsSheet = '2026::23.04.26 - Kids Room';
+
+const tableQueries = [
+  {
+    tableRange: '17:18',
+    name: 'TopImageTitle',
+  },
+  {
+    tableRange: '20',
+    name: 'intro',
+  },
+  // {
+  //   tableRange: '25:28',
+  //   name: 'paragraphs',
+  // },
+  {
+    tableRange: '21:24',
+    name: 'categories',
+  },
+  {
+    tableRange: '25:26',
+    name: 'condition',
+  },
+  {
+    tableRange: '14:15',
+    name: 'timer',
+    tableName: '2026::02.03.26 - March Peak Start',
+  },
+];
+
+const links = {
+  TopImageTitle_href: translateLink({ value: 'content/lp26-04-23' }),
+  TopImageTitle_src: translateImage({ value: '20260423_01.png' }),
+
+  TopImage_src: getImageUrl('20260423_Pic.png', true),
+  TopImage_href: translateLink({ value: 'content/lp26-04-23' }),
+
+  Banner_1: translateLink({ value: 'content/lp26-04-17' }),
+  Banner_1_Image: translateImage({ value: '20260417b.png' }),
+
+  Banner_2: translateLink({ value: 'content/lp26-04-16' }),
+  Banner_2_Image: translateImage({ value: '20260416b.png' }),
+
+  Timer_href: translateLink({ value: 'content/lp26-04-24' }),
+};
+
+const TopImageTitle_data = {
+  color: '#000000',
+  backgroundColor: '#FFEFD9',
+  type: 'twoSameLines',
+};
+
+const categories = [
+  {
+    name: 'Kids decor',
+    src: getImageUrl('20260423_Cat00.png', true),
+    href: 'https://www.beliani.ch/children-room/kids-decor/',
+    background: '#FFEFD9',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '60',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // YAMBA
+      {
+        id: 703620,
+        src: getImageUrl('20260423_Pic01.png', true),
+      },
+      // BAGNU
+      {
+        id: 563339,
+        src: getImageUrl('20260423_Pic02.png', true),
+      },
+      // BEMBA
+      {
+        id: 346059,
+        src: getImageUrl('20260423_Pic03.png', true),
+      },
+      // HAINAN
+      {
+        id: 438712,
+        src: getImageUrl('20260423_Pic04.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Kids beds',
+    src: getImageUrl('20260423_Cat10.png', true),
+    href: 'https://www.beliani.ch/children-room/kids-beds/',
+    background: '#EAD9D9',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '35',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // GRISETOT
+      {
+        id: 712030,
+        src: getImageUrl('20260423_Pic11.png', true),
+      },
+      // CHOCQUES
+      {
+        id: 695972,
+        src: getImageUrl('20260423_Pic12.png', true),
+      },
+      // ANAIS
+      {
+        id: 686766,
+        src: getImageUrl('20260423_Pic13.png', true),
+      },
+      // ORGEVAL
+      {
+        id: 594970,
+        src: getImageUrl('20260423_Pic14.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Kids storage',
+    src: getImageUrl('20260423_Cat20.png', true),
+    href: 'https://www.beliani.ch/children-room/kids-storage/',
+    background: '#FFEFD9',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '35',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // COSMOS
+      {
+        id: 698724,
+        src: getImageUrl('20260423_Pic21.png', true),
+      },
+      // JACKIT
+      {
+        id: 681714,
+        src: getImageUrl('20260423_Pic22.png', true),
+      },
+      // QUILPIE
+      {
+        id: 680423,
+        src: getImageUrl('20260423_Pic23.png', true),
+      },
+      // CLOUDY
+      {
+        id: 571524,
+        src: getImageUrl('20260423_Pic24.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Kids lighting',
+    src: getImageUrl('20260423_Cat30.png', true),
+    href: 'https://www.beliani.ch/children-room/kids-lighting/',
+    background: '#EAD9D9',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '35',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // STRAWBERRY
+      {
+        id: 393874,
+        src: getImageUrl('20260423_Pic31.png', true),
+      },
+      // ORCHON
+      {
+        id: 637729,
+        src: getImageUrl('20260423_Pic32.png', true),
+      },
+      // ABBIE
+      {
+        id: 432533,
+        src: getImageUrl('20260423_Pic33.png', true),
+      },
+      // DOUCE
+      {
+        id: 343763,
+        src: getImageUrl('20260423_Pic34.png', true),
+      },
+    ],
+  },
+];
+
+const timer = {
+  // freebies: getImageUrl('20260416free.png', true),
+  image: {
+    CHDE: 'https://gen.sendtric.com/countdown/didfbq2i1q',
+    CHFR: 'https://gen.sendtric.com/countdown/r71zf1jd11',
+    FR: 'https://gen.sendtric.com/countdown/sxqgzkw8u5',
+    DE: 'https://gen.sendtric.com/countdown/lm7x8plxba',
+    UK: 'https://gen.sendtric.com/countdown/f8p39a3vof',
+    AT: 'https://gen.sendtric.com/countdown/fvjl4a0wlo',
+    ES: 'https://gen.sendtric.com/countdown/mojee85559',
+    PL: 'https://gen.sendtric.com/countdown/rrbj4ven2x',
+    NL: 'https://gen.sendtric.com/countdown/j97ll6bh7c',
+    PT: 'https://gen.sendtric.com/countdown/b700i7di6w',
+    IT: 'https://gen.sendtric.com/countdown/rbi9x0clln',
+    SE: 'https://gen.sendtric.com/countdown/7dax2s0h7f',
+    HU: 'https://gen.sendtric.com/countdown/1v16m4qajv',
+    DK: 'https://gen.sendtric.com/countdown/betlxlxz9o',
+    CZ: 'https://gen.sendtric.com/countdown/ul1m7yqf2c',
+    FI: 'https://gen.sendtric.com/countdown/g4buvgxqq5',
+    NO: 'https://gen.sendtric.com/countdown/i8b2xbuxp7',
+    SK: 'https://gen.sendtric.com/countdown/q5l9kg3o7h',
+    BENL: 'https://gen.sendtric.com/countdown/hqlox8talt',
+    BEFR: 'https://gen.sendtric.com/countdown/hauel1wn6f',
+    RO: 'https://gen.sendtric.com/countdown/8oxikkdvml',
+  },
+};
+
+export default new entities.Campaign({
+  startId: 42829,
+  name: 'Kids room',
+  date: '23.04.2026',
+  issueCardId: 463445,
+  lpId: 29689,
+  alarm: {
+    isActive: false,
+  },
+  isArchive: false,
+  optimizeImg: false,
+  figmaUrl: 'https://www.figma.com/design/k6EA69v8tjldNumZsurmtj/',
+  templates: [
+    {
+      name: 'Newsletter',
+      type: types.NEWSLETTER,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.NS,
+      translationsSpreadsheet: campaignTranslationsSheet,
+      background: '#FFEFD9',
+      color: '#000000',
+      wrapper: types.WRAPPER,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      timer: timer,
+      intro: {
+        color: '#000000', // unused
+        backgroundColor: '#FFEFD9', // unused
+        alignment: 'left',
+        type: 'paragraph',
+      },
+      Inside: {
+        color: '#ffffff',
+        backgroundColor: '#FF2F00',
+        type: 'timer',
+      },
+    },
+    {
+      name: 'Landing',
+      type: types.LANDINGPAGE,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.LP,
+      background: '#FFEFD9',
+      color: '#000000',
+      translationsSpreadsheet: campaignTranslationsSheet,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      timer: timer,
+      intro: {
+        color: '#000000', // unused
+        backgroundColor: '#FFEFD9', // unused
+        alignment: 'left',
+        type: 'paragraph',
+      },
+      Inside: {
+        color: '#ffffff',
+        backgroundColor: '#FF2F00',
+        type: 'timer',
+      },
+    },
+  ],
+});
