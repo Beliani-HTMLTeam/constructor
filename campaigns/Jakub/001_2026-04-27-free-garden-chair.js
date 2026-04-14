d6683060956ba70c3d8f057cea39d0f872aca1a5
RENEGADES-MAL <jakub.gajek@vp.pl>
2026-04-14 08:29:39 +0200

    feat: approved nslt templates added to Jakub's folder

---
+++ campaigns/Jakub/001_2026-04-27-free-garden-chair.js
@@ -0,0 +1,323 @@
+// Campaign generated from form
+const campaignTranslationsSheet = 'Voucher - 27.04.26 - Free Garden Chair';
+
+const tableQueries = [
+  {
+    tableRange: '22:23',
+    name: 'TopImageTitle',
+  },
+  {
+    name: 'offer',
+    tableRange: '25:27',
+  },
+  {
+    name: 'offer_date',
+    tableRange: '29',
+  },
+  {
+    name: 'intro',
+    tableRange: '32:33',
+  },
+  {
+    tableRange: '45:46',
+    name: 'condition',
+  },
+  {
+    tableRange: '35:38',
+    name: 'paragraphs',
+  },
+];
+
+const links = {
+  TopImageTitle_href: translateLink({ value: 'content/lp26-04-27' }),
+  TopImageTitle_src: translateImage({ value: '20260427_01.png' }),
+
+  TopImage_src: getImageUrl('20260427_Gif.gif', true),
+  TopImage_href: translateLink({ value: 'content/lp26-04-27' }),
+
+  Banner_1: translateLink({ value: 'content/lp26-04-16' }),
+  Banner_1_Image: translateImage({ value: '20260416b.png' }),
+
+  Banner_2: translateLink({ value: 'content/lp26-04-15' }),
+  Banner_2_Image: translateImage({ value: '20260415b.png' }),
+};
+
+const TopImageTitle_data = {
+  color: '#000000',
+  backgroundColor: '#F6E7E6',
+  type: 'standard',
+};
+
+const categories = [
+  // freebiesy
+  {
+    paragraph: {
+      spaceAfter: '',
+    },
+    paddingTop: '0',
+    type: 'freebies',
+    background: '#F6E7E6',
+    color: '#000000',
+    spaceAfter: "newsletterBottom45px",
+    freebies: [[
+      // MARCEDDI
+      {
+        id: '517685',
+        src: getImageUrl('20260427_Pic02.png', true),
+      },
+      // CAMPO
+      {
+        id: '71178',
+        src: getImageUrl('20260427_Pic03.png', true),
+      },
+      // MARCEDDI
+      {
+        id: '517640',
+        src: getImageUrl('20260427_Pic01.png', true),
+      },
+    ]],
+  },
+
+  // klasyczki
+  {
+    name: 'Dining sets',
+    src: getImageUrl('20260427_Cat10.png', true),
+    href: 'https://www.beliani.ch/garden-furniture/outdoor-furniture/garden-dining-sets/',
+    background: '#FFCCB7',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '45',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // JACO/SUMIRAGO
+      {
+        id: '700038',
+        src: getImageUrl('20260427_Pic11.png', true),
+      },
+      // CASCAIS
+      {
+        id: '502654',
+        src: getImageUrl('20260427_Pic12.png', true),
+      },
+      // NOTTERI
+      {
+        id: '621129',
+        src: getImageUrl('20260427_Pic13.png', true),
+      },
+      // MALETTO/GROSSETO
+      {
+        id: '525622',
+        src: getImageUrl('20260427_Pic14.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Garden Tables',
+    src: getImageUrl('20260427_Cat20.png', true),
+    href: 'https://www.beliani.ch/outdoor-furniture/garden-tables/',
+    background: '#F6E7E6',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '35',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // SIRMIONE
+      {
+        id: '709805',
+        src: getImageUrl('20260427_Pic21.png', true),
+      },
+      // TOLENTINO
+      {
+        id: '682540',
+        src: getImageUrl('20260427_Pic22.png', true),
+      },
+      // SOVARETO
+      {
+        id: '651866',
+        src: getImageUrl('20260427_Pic23.png', true),
+      },
+      // SPOLETO
+      {
+        id: '709747',
+        src: getImageUrl('20260427_Pic24.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Bistro Sets',
+    src: getImageUrl('20260427_Cat30.png', true),
+    href: 'https://www.beliani.ch/garden-furniture/outdoor-furniture/balcony-furniture/',
+    background: '#FFCCB7',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '35',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // CAMPRENA
+      {
+        id: '506444',
+        src: getImageUrl('20260427_Pic31.png', true),
+      },
+      // LENOLA
+      {
+        id: '293980',
+        src: getImageUrl('20260427_Pic32.png', true),
+      },
+      // GARGANO PE
+      {
+        id: '385843',
+        src: getImageUrl('20260427_Pic33.png', true),
+      },
+      // RIVAROTTA
+      {
+        id: '622051',
+        src: getImageUrl('20260427_Pic34.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Outdoor Kitchen',
+    src: getImageUrl('20260427_Cat40.png', true),
+    href: 'https://www.beliani.ch/garden-furniture/outdoor-furniture/outdoor-kitchen/',
+    background: '#F6E7E6',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '35',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // VENOSA
+      {
+        id: '618287',
+        src: getImageUrl('20260427_Pic41.png', true),
+      },
+      // MAENZA
+      {
+        id: '616263',
+        src: getImageUrl('20260427_Pic42.png', true),
+      },
+      // MIRADOR PE
+      {
+        id: '623173',
+        src: getImageUrl('20260427_Pic43.png', true),
+      },
+      // IVREA
+      {
+        id: '624619',
+        src: getImageUrl('20260427_Pic44.png', true),
+      },
+    ],
+  },
+];
+
+export default new entities.Campaign({
+  startId: '42893',
+  name: 'Free garden chair',
+  date: '27.04.2026',
+  issueCardId: '463732',
+  lpId: '29727',
+  alarm: {
+    isActive: false,
+  },
+  isArchive: false,
+  optimizeImg: false,
+  figmaUrl: 'https://www.figma.com/design/fT3GFKWV5vDEuWXhtnyd5t/Newsletter-Free-garden-chair---Monday-2026.04.27--Copy-?node-id=0-1&t=9f5UpsZ8IQZEknCx-1',
+  templates: [
+    {
+      background: '#F6E7E6',
+      color: '#000000',
+      template: templates.Thursday, // User should change this
+
+      intro: {
+        color: '#000000', // unused
+        backgroundColor: '#FFCCB7', // unused
+        alignment: 'left',
+        position: 'afterFreebies',
+      },
+      css: types.CSS.NS,
+      name: 'Newsletter',
+      type: types.NEWSLETTER,
+      translationsSpreadsheet: campaignTranslationsSheet,
+      wrapper: types.WRAPPER,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+    },
+    {
+      background: '#F6E7E6',
+      color: '#000000',
+      template: templates.Thursday, // User should change this
+
+      intro: {
+        color: '#000000', // unused
+        backgroundColor: '#FFCCB7', // unused
+        alignment: 'left',
+        position: 'afterFreebies',
+      },
+      css: types.CSS.LP,
+      name: 'Landing',
+      type: types.LANDINGPAGE,
+      translationsSpreadsheet: campaignTranslationsSheet,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+    },
+  ],
+});
---
+++ campaigns/Jakub/002_2026-05-04-free-mirror.js
@@ -0,0 +1,338 @@
+// Campaign generated from form
+const campaignTranslationsSheet = 'Voucher - 04.05.26 - Free Mirror';
+
+const tableQueries = [
+  {
+    tableRange: '21:22',
+    name: 'TopImageTitle',
+  },
+  {
+    name: 'offer',
+    tableRange: '24:26',
+  },
+  {
+    name: 'offer_date',
+    tableRange: '28',
+  },
+  {
+    name: 'intro',
+    tableRange: '29:30',
+  },
+  {
+    tableRange: '34:35',
+    name: 'condition',
+  },
+  {
+    tableRange: '35:38',
+    name: 'paragraphs',
+  },
+];
+
+const links = {
+  TopImageTitle_href: translateLink({ value: 'content/lp26-05-04' }),
+  TopImageTitle_src: translateImage({ value: '20260504_01.png' }),
+
+  TopImage_src: getImageUrl('20260504_Gif.gif', true),
+  TopImage_href: translateLink({ value: 'content/lp26-05-04' }),
+
+  Banner_1: translateLink({ value: 'content/lp26-04-29' }),
+  Banner_1_Image: translateImage({ value: '20260429b.png' }),
+
+  Banner_2: translateLink({ value: 'content/lp26-04-23' }),
+  Banner_2_Image: translateImage({ value: '20260423b.png' }),
+};
+
+const TopImageTitle_data = {
+  color: '#000000',
+  backgroundColor: '#FFE8DE',
+  type: 'standard',
+};
+
+const categories = [
+  // freebiesy
+  {
+    paragraph: {
+      spaceAfter: '',
+    },
+    paddingTop: '0',
+    type: 'freebies',
+    background: '#FFE8DE',
+    color: '#000000',
+    spaceAfter: "newsletterBottom45px",
+    freebies: [[
+      // YEBRA
+      {
+        id: '324994',
+        src: getImageUrl('20260504_pic01.png', true),
+      },
+      // PASAKU
+      {
+        id: '314370',
+        src: getImageUrl('20260504_pic02.png', true),
+      },
+      // SOLEDAD
+      {
+        id: '369238',
+        src: getImageUrl('20260504_pic03.png', true),
+      },
+      // DOMME
+      {
+        id: '345545',
+        src: getImageUrl('20260504_pic04.png', true),
+      },
+      //TIERGA
+      {
+        id: '325032',
+        src: getImageUrl('20260504_pic05.png', true),
+      },
+      // BABAI
+      {
+        id: '314272',
+        src: getImageUrl('20260504_pic06.png', true),
+      },
+    ]],
+  },
+
+  // klasyczki
+  {
+    name: 'Sofas',
+    src: getImageUrl('20260504_Cat10.png', true),
+    href: 'https://www.beliani.ch/living-room-furniture/sofas/',
+    background: '#fecd8c',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '45',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // VINTERBRO
+      {
+        id: '442870',
+        src: getImageUrl('20260504_Pic11.png', true),
+      },
+      // ASBY
+      {
+        id: '231617',
+        src: getImageUrl('20260504_Pic12.png', true),
+      },
+      // CHESTERFIELD
+      {
+        id: '214089',
+        src: getImageUrl('20260504_Pic13.png', true),
+      },
+      // ELVENES
+      {
+        id: '101669',
+        src: getImageUrl('20260504_Pic14.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Armchairs',
+    src: getImageUrl('20260504_Cat20.png', true),
+    href: 'https://www.beliani.ch/living-room-furniture/armchairs/',
+    background: '#F6E7E6',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '35',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // EVJE
+      {
+        id: '515945',
+        src: getImageUrl('20260504_Pic21.png', true),
+      },
+      // NOLVIK
+      {
+        id: '597308',
+        src: getImageUrl('20260504_Pic22.png', true),
+      },
+      // ILBRO
+      {
+        id: '618024',
+        src: getImageUrl('20260504_Pic23.png', true),
+      },
+      // ALVESTA
+      {
+        id: '403367',
+        src: getImageUrl('20260504_Pic24.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Chairs',
+    src: getImageUrl('20260504_Cat30.png', true),
+    href: 'https://www.beliani.ch/dining-room-furniture/chairs/',
+    background: '#fecd8c',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '35',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // DIXON
+      {
+        id: '575666',
+        src: getImageUrl('20260504_Pic31.png', true),
+      },
+      // CORDOVA
+      {
+        id: '424428',
+        src: getImageUrl('20260504_Pic32.png', true),
+      },
+      // ISANTI
+      {
+        id: '569273',
+        src: getImageUrl('20260504_Pic33.png', true),
+      },
+      // MYLO
+      {
+        id: '575229',
+        src: getImageUrl('20260504_Pic34.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Pouffes',
+    src: getImageUrl('20260504_Cat40.png', true),
+    href: 'https://www.beliani.ch/living-room-furniture/stools/',
+    background: '#F6E7E6',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: '35',
+    title: {
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      align: 'left',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: true,
+      name: true,
+    },
+    products: [
+      // DINNI
+      {
+        id: '428207',
+        src: getImageUrl('20260504_Pic41.png', true),
+      },
+      // KANSAS
+      {
+        id: '512750',
+        src: getImageUrl('20260504_Pic42.png', true),
+      },
+      // MUKKI
+      {
+        id: '101143',
+        src: getImageUrl('20260504_Pic43.png', true),
+      },
+      // VIRGINIA
+      {
+        id: '171419',
+        src: getImageUrl('20260504_Pic44.png', true),
+      },
+    ],
+  },
+];
+
+export default new entities.Campaign({
+  startId: '42957',
+  name: 'Free mirror',
+  date: '04.05.2026',
+  issueCardId: '470531',
+  lpId: '29765',
+  alarm: {
+    isActive: false,
+  },
+  isArchive: false,
+  optimizeImg: false,
+  figmaUrl: 'https://www.figma.com/design/fT3GFKWV5vDEuWXhtnyd5t/https://www.figma.com/design/c2nfd2iNss24wW7t4CaYh6/2026.05.04---Newsletter---Free-Mirror--Copy-?node-id=0-1&t=gGhMlKu26q8ES8Pg-1-Free-garden-chair---Monday-2026.04.27--Copy-?node-id=0-1&t=9f5UpsZ8IQZEknCx-1',
+  templates: [
+    {
+      background: '#F6E7E6',
+      color: '#000000',
+      template: templates.Thursday, // User should change this
+
+      intro: {
+        color: '#000000', // unused
+        backgroundColor: '#fecd8c', // unused
+        alignment: 'left',
+        position: 'afterFreebies',
+      },
+      css: types.CSS.NS,
+      name: 'Newsletter',
+      type: types.NEWSLETTER,
+      translationsSpreadsheet: campaignTranslationsSheet,
+      wrapper: types.WRAPPER,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+    },
+    {
+      background: '#F6E7E6',
+      color: '#000000',
+      template: templates.Thursday, // User should change this
+
+      intro: {
+        color: '#000000', // unused
+        backgroundColor: '#fecd8c', // unused
+        alignment: 'left',
+        position: 'afterFreebies',
+      },
+      css: types.CSS.LP,
+      name: 'Landing',
+      type: types.LANDINGPAGE,
+      translationsSpreadsheet: campaignTranslationsSheet,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+    },
+  ],
+});
