58b59fee644fe2fa5178c084a02bb6f0136fd8f8
kkazzann <kamil.kazaniecki@beliani.net>
2026-04-23 08:08:49 +0200

    feat: add 07.05 camp

---
+++ campaigns/KamilK/038_2026-05-07-dining-room-styles.js
@@ -0,0 +1,375 @@
+// Campaign generated from form
+const campaignTranslationsSheet = '2026::07.05.26 - Dining Room Styles';
+
+const tableQueries = [
+  {
+    tableRange: '17:18',
+    name: 'TopImageTitle',
+  },
+  {
+    tableRange: '20',
+    name: 'intro',
+  },
+  {
+    tableRange: '25:28',
+    name: 'paragraphs',
+  },
+  {
+    tableRange: '21:24',
+    name: 'categories',
+  },
+  {
+    tableRange: '29:30',
+    name: 'condition',
+  },
+  {
+    tableRange: '14:15',
+    name: 'timer',
+    tableName: '2026::Voucher - 29.04.26 - French Day',
+  },
+];
+
+const links = {
+  TopImageTitle_href: translateLink({ value: 'content/lp26-05-07' }),
+  TopImageTitle_src: translateImage({ value: '20260507_01.png' }),
+
+  TopImage_src: getImageUrl('20260507_Gif.gif', true),
+  TopImage_href: translateLink({ value: 'content/lp26-04-30' }),
+
+  Intro_cta_href: 'https://www.beliani.ch/dining-room-furniture/',
+
+  Timer_href: translateLink({ value: 'content/lp26-04-29' }),
+
+  Banner_1: translateLink({ value: 'content/lp26-05-01' }),
+  Banner_1_Image: translateImage({ value: '20260501b.png' }),
+
+  Banner_2: translateLink({ value: 'content/lp26-04-30' }),
+  Banner_2_Image: translateImage({ value: '20260430b.png' }),
+};
+
+const TopImageTitle_data = {
+  color: '#ffffff',
+  backgroundColor: '#750000',
+  type: 'twoSameLines',
+};
+
+const categories = [
+  {
+    // name: 'Living room',
+    // src: getImageUrl('20260507_Cat00.png', true),
+    href: 'https://www.beliani.co.uk/meet-beliani/modern-classic-style/',
+    background: '#750000',
+    color: '#ffffff',
+    type: '4prods-cat',
+    cta: true,
+    paddingTop: '60',
+    insideContainer: false,
+    imageSide: 'right',
+    title: {
+      align: 'center',
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      // position: 'afterProducts',
+      align: 'center',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      gapBetweenHorizontal: false,
+      gapBetweenVertical: true,
+      prices: false,
+      name: false,
+    },
+    products: [
+      {
+        id: 648472,
+        src: getImageUrl('20260507_Pic01.png', true),
+        categorySrc: getImageUrl('20260507_Cat01.png', true),
+        spaceAfter: '',
+      },
+      {
+        id: 573957,
+        src: getImageUrl('20260507_Pic02.png', true),
+        categorySrc: getImageUrl('20260507_Cat02.png', true),
+        spaceAfter: '',
+      },
+      {
+        id: 585205,
+        src: getImageUrl('20260507_Pic03.png', true),
+        categorySrc: getImageUrl('20260507_Cat03.png', true),
+        spaceAfter: '',
+      },
+      {
+        id: 469011,
+        src: getImageUrl('20260507_Pic04.png', true),
+        categorySrc: getImageUrl('20260507_Cat04.png', true),
+      },
+    ],
+  },
+  {
+    // name: 'Bedroom',
+    // src: getImageUrl('20260507_Cat10.png', true),
+    href: 'https://www.beliani.ch/travel-with-beliani/portugal/',
+    background: '#FFD6C5',
+    color: '#000000',
+    type: '4prods-cat',
+    cta: true,
+    paddingTop: '35',
+    imageSide: 'right',
+    insideContainer: false,
+    title: {
+      align: 'center',
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      // position: 'afterProducts',
+      align: 'center',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      gapBetweenHorizontal: false,
+      gapBetweenVertical: true,
+      prices: false,
+      name: false,
+    },
+    products: [
+      {
+        id: 576378,
+        src: getImageUrl('20260507_Pic11.png', true),
+        categorySrc: getImageUrl('20260507_Cat11.png', true),
+        spaceAfter: '',
+      },
+      {
+        id: 460172,
+        src: getImageUrl('20260507_Pic12.png', true),
+        categorySrc: getImageUrl('20260507_Cat12.png', true),
+        spaceAfter: '',
+      },
+      {
+        id: 334650,
+        src: getImageUrl('20260507_Pic13.png', true),
+        categorySrc: getImageUrl('20260507_Cat13.png', true),
+        spaceAfter: '',
+      },
+      {
+        id: 363701,
+        src: getImageUrl('20260507_Pic14.png', true),
+        categorySrc: getImageUrl('20260507_Cat14.png', true),
+      },
+    ],
+  },
+  {
+    // name: 'Dining room',
+    // src: getImageUrl('20260507_Cat20.png', true),
+    href: 'https://www.beliani.co.uk/trends/soft-minimalism-style/',
+    background: '#750000',
+    color: '#ffffff',
+    type: '4prods-cat',
+    cta: true,
+    paddingTop: '35',
+    imageSide: 'right',
+    insideContainer: false,
+    title: {
+      align: 'center',
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      // position: 'afterProducts',
+      align: 'center',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      gapBetweenHorizontal: false,
+      gapBetweenVertical: true,
+      prices: false,
+      name: false,
+    },
+    products: [
+      {
+        id: 243876,
+        src: getImageUrl('20260507_Pic21.png', true),
+        categorySrc: getImageUrl('20260507_Cat21.png', true),
+        spaceAfter: '',
+      },
+      {
+        id: 398859,
+        src: getImageUrl('20260507_Pic22.png', true),
+        categorySrc: getImageUrl('20260507_Cat22.png', true),
+        spaceAfter: '',
+      },
+      {
+        id: 595292,
+        src: getImageUrl('20260507_Pic23.png', true),
+        categorySrc: getImageUrl('20260507_Cat23.png', true),
+        spaceAfter: '',
+      },
+      {
+        id: 398206,
+        src: getImageUrl('20260507_Pic24.png', true),
+        categorySrc: getImageUrl('20260507_Cat24.png', true),
+      },
+    ],
+  },
+  {
+    // name: 'Bathroom',
+    // src: getImageUrl('20260507_Cat30.png', true),
+    href: 'https://www.beliani.ch/trends/desert/',
+    background: '#FFD6C5',
+    color: '#000000',
+    type: '4prods-cat',
+    cta: true,
+    paddingTop: '35',
+    imageSide: 'right',
+    insideContainer: false,
+    title: {
+      align: 'center',
+      show: true,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      // position: 'afterProducts',
+      align: 'center',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      gapBetweenHorizontal: false,
+      gapBetweenVertical: true,
+      prices: false,
+      name: false,
+    },
+    products: [
+      {
+        id: 212667,
+        src: getImageUrl('20260507_Pic31.png', true),
+        categorySrc: getImageUrl('20260507_Cat31.png', true),
+        spaceAfter: '',
+      },
+      {
+        id: 409109,
+        src: getImageUrl('20260507_Pic32.png', true),
+        categorySrc: getImageUrl('20260507_Cat32.png', true),
+        spaceAfter: '',
+      },
+      {
+        id: 426100,
+        src: getImageUrl('20260507_Pic33.png', true),
+        categorySrc: getImageUrl('20260507_Cat33.png', true),
+        spaceAfter: '',
+      },
+      {
+        id: 401777,
+        src: getImageUrl('20260507_Pic34.png', true),
+        categorySrc: getImageUrl('20260507_Cat34.png', true),
+      },
+    ],
+  },
+];
+
+const timer = {
+  // freebies: getImageUrl('20260507free.png', true),
+  image: {
+    CHDE: 'https://gen.sendtric.com/countdown/hw5bqmc9fa',
+    CHFR: 'https://gen.sendtric.com/countdown/k5hz7javwj',
+    FR: 'https://gen.sendtric.com/countdown/zt0fxop1fj',
+    DE: 'https://gen.sendtric.com/countdown/16kxn6elvf',
+    UK: 'https://gen.sendtric.com/countdown/g1yb3ub0lm',
+    AT: 'https://gen.sendtric.com/countdown/qc7gginkgb',
+    ES: 'https://gen.sendtric.com/countdown/nx56j4xpqx',
+    PL: 'https://gen.sendtric.com/countdown/5zbduvnv20',
+    NL: 'https://gen.sendtric.com/countdown/l734y1a60j',
+    PT: 'https://gen.sendtric.com/countdown/0ycq3se6o4',
+    IT: 'https://gen.sendtric.com/countdown/odujbuo7zm',
+    SE: 'https://gen.sendtric.com/countdown/5ah5zrieg8',
+    HU: 'https://gen.sendtric.com/countdown/dqvkoxsd1e',
+    DK: 'https://gen.sendtric.com/countdown/yz6m2pg9lr',
+    CZ: 'https://gen.sendtric.com/countdown/a0royu6nzp',
+    FI: 'https://gen.sendtric.com/countdown/96j9ecym4w',
+    NO: 'https://gen.sendtric.com/countdown/tmvoborzj7',
+    SK: 'https://gen.sendtric.com/countdown/d62irf6zeb',
+    BENL: 'https://gen.sendtric.com/countdown/lk143xowlz',
+    BEFR: 'https://gen.sendtric.com/countdown/x2ykmydl87',
+    RO: 'https://gen.sendtric.com/countdown/5787v7yxvl',
+  },
+};
+
+export default new entities.Campaign({
+  startId: 43502,
+  name: 'Dining room styles',
+  date: '07.05.2026',
+  issueCardId: 474517,
+  lpId: 30025,
+  alarm: {
+    isActive: false,
+  },
+  isArchive: false,
+  optimizeImg: false,
+  figmaUrl: 'https://www.figma.com/design/vowo8ZZ6bBTxwe53Inty0U/',
+  templates: [
+    {
+      name: 'Newsletter',
+      type: types.NEWSLETTER,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.NS,
+      translationsSpreadsheet: campaignTranslationsSheet,
+      background: '#750000',
+      color: '#ffffff',
+      wrapper: types.WRAPPER,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      timer: timer,
+      intro: {
+        cta: {
+          show: true,
+          spaceAfter: 'newsletterBottom35px',
+        },
+        color: '#ffffff', // unused
+        backgroundColor: '#750000', // unused
+        alignment: 'center',
+        type: 'paragraph',
+      },
+      Inside: {
+        color: '#000000',
+        backgroundColor: '#FD9000',
+        type: 'timer',
+      },
+    },
+    {
+      name: 'Landing',
+      type: types.LANDINGPAGE,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.LP,
+      background: '#750000',
+      color: '#ffffff',
+      translationsSpreadsheet: campaignTranslationsSheet,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      timer: timer,
+      intro: {
+        cta: {
+          show: true,
+          spaceAfter: 'newsletterBottom35px',
+        },
+        color: '#ffffff', // unused
+        backgroundColor: '#750000', // unused
+        alignment: 'center',
+        type: 'paragraph',
+      },
+      Inside: {
+        color: '#000000',
+        backgroundColor: '#FD9000',
+        type: 'timer',
+      },
+    },
+  ],
+});
