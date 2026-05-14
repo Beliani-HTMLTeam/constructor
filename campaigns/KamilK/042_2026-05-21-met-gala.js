511f79eb313b6ae7182ec821aa460c44a33c7a90
kkazzann <kamil.kazaniecki@beliani.net>
2026-05-14 11:04:19 +0200

    feat: add met gala campaign

---
+++ campaigns/KamilK/042_2026-05-21-met-gala.js
@@ -0,0 +1,326 @@
+// Campaign generated from form
+const campaignTranslationsSheet = '2026::21.05.26 - Met Gala';
+
+const tableQueries = [
+  {
+    tableRange: '16:17',
+    name: 'TopImageTitle',
+  },
+  {
+    tableRange: '19',
+    name: 'intro',
+  },
+  {
+    tableRange: '20:23',
+    name: 'paragraphs',
+  },
+  {
+    tableRange: '24:25',
+    name: 'condition',
+  },
+  // {
+  //   tableRange: '15:16',
+  //   name: 'timer',
+  //   tableName: '2026::16.03.26 - Free Gift',
+  // },
+];
+
+const links = {
+  TopImageTitle_href: translateLink({ value: 'content/lp26-05-21' }),
+  TopImageTitle_src: translateImage({ value: '20260521_01.png' }),
+
+  TopImage_src: getImageUrl('20260521_Gif.gif', true),
+  TopImage_href: translateLink({ value: 'content/lp26-05-21' }),
+
+  Banner_1: translateLink({ value: 'content/lp26-05-15' }),
+  Banner_1_Image: translateImage({ value: '20260515b.png' }),
+
+  Banner_2: translateLink({ value: 'content/lp26-05-14' }),
+  Banner_2_Image: translateImage({ value: '20260514b.png' }),
+
+  // Timer_href: translateLink({ value: 'content/lp26-03-16' }),
+};
+
+const TopImageTitle_data = {
+  color: '#000000',
+  backgroundColor: '#FFF0EA',
+  type: 'twoSameLines',
+};
+
+const categories = [
+  {
+    // name: 'Dining room',
+    src: getImageUrl('20260521_Cat00.jpg', true),
+    href: 'https://www.beliani.ch/sofas/',
+    background: '#FFF0EA',
+    color: '#000000',
+    type: '1Big-2Vertical',
+    cta: true,
+    paddingTop: '0',
+    title: {
+      show: false,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'center',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: false,
+      name: false,
+    },
+    products: [
+      // EGEDAAL - big img left
+      {
+        id: '710341',
+        src: getImageUrl('20260521_Cat01.jpg', true),
+        useCategoryLink: true,
+        // spaceAfter: '0',
+      },
+      // EGEDAAL
+      {
+        id: '710341',
+        src: getImageUrl('20260521_Pic01.png', true),
+        // spaceAfter: '0',
+      },
+      // LAERDAL
+      {
+        id: '721173',
+        src: getImageUrl('20260521_Pic02.png', true),
+        // spaceAfter: '0',
+      },
+    ],
+  },
+
+  {
+    // name: 'Dining room',
+    src: getImageUrl('20260521_Cat10.jpg', true),
+    href: 'https://www.beliani.ch/sofas/',
+    background: '#FFF0EA',
+    color: '#000000',
+    type: '1Big-2Vertical',
+    cta: true,
+    paddingTop: '0',
+    title: {
+      show: false,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'center',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: false,
+      name: false,
+    },
+    products: [
+      // FAABORG - big img left
+      {
+        id: '713500',
+        src: getImageUrl('20260521_Cat11.jpg', true),
+        useCategoryLink: true,
+        // spaceAfter: '0',
+      },
+      // FAABORG
+      {
+        id: '713500',
+        src: getImageUrl('20260521_Pic11.png', true),
+        // spaceAfter: '0',
+      },
+      // LEIREN
+      {
+        id: '680442',
+        src: getImageUrl('20260521_Pic12.png', true),
+        // spaceAfter: '0',
+      },
+    ],
+  },
+
+  {
+    // name: 'Dining room',
+    src: getImageUrl('20260521_Cat20.jpg', true),
+    href: 'https://www.beliani.ch/sofas/',
+    background: '#FFF0EA',
+    color: '#000000',
+    type: '1Big-2Vertical',
+    cta: true,
+    paddingTop: '0',
+    title: {
+      show: false,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'center',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: false,
+      name: false,
+    },
+    products: [
+      // SAVASSE - big img left
+      {
+        id: '	646569',
+        src: getImageUrl('20260521_Cat21.jpg', true),
+        useCategoryLink: true,
+        // spaceAfter: '0',
+      },
+      // SAVASSE
+      {
+        id: '646569',
+        src: getImageUrl('20260521_Pic21.png', true),
+        // spaceAfter: '0',
+      },
+      // NANTILLY
+      {
+        id: '	637501',
+        src: getImageUrl('20260521_Pic22.png', true),
+        // spaceAfter: '0',
+      },
+
+    ],
+  },
+
+  {
+    // name: 'Dining room',
+    src: getImageUrl('20260521_Cat30.jpg', true),
+    href: 'https://www.beliani.ch/sofas/',
+    background: '#FFF0EA',
+    color: '#000000',
+    type: '1Big-2Vertical',
+    cta: true,
+    paddingTop: '0',
+    title: {
+      show: false,
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'center',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      prices: false,
+      name: false,
+    },
+    products: [
+      // NACKA - big img left
+      {
+        id: '	715252',
+        src: getImageUrl('20260521_Cat31.jpg', true),
+        useCategoryLink: true,
+        // spaceAfter: '0',
+      },
+      // NACKA
+      {
+        id: '715252',
+        src: getImageUrl('20260521_Pic31.png', true),
+        // spaceAfter: '0',
+      },
+      // LEMVIG
+      {
+        id: '	710893',
+        src: getImageUrl('20260521_Pic32.png', true),
+        // spaceAfter: '0',
+      },
+
+    ],
+  },
+];
+
+// const timer = {
+//   freebies: getImageUrl('20260521_Free.png', true),
+//   image: {
+//     CHDE: 'https://gen.sendtric.com/countdown/gfvwy5vjj1',
+//     CHFR: 'https://gen.sendtric.com/countdown/zjlnud4zad',
+//     FR: 'https://gen.sendtric.com/countdown/3yvaazm6gw',
+//     DE: 'https://gen.sendtric.com/countdown/mcgb10x3lm',
+//     UK: 'https://gen.sendtric.com/countdown/yibart2xx1',
+//     AT: 'https://gen.sendtric.com/countdown/ljb5jk5hks',
+//     ES: 'https://gen.sendtric.com/countdown/ksqld5jpwj',
+//     PL: 'https://gen.sendtric.com/countdown/r5d62v9qb8',
+//     NL: 'https://gen.sendtric.com/countdown/m5axoo2d33',
+//     PT: 'https://gen.sendtric.com/countdown/6sdiys14sg',
+//     IT: 'https://gen.sendtric.com/countdown/1zmhbgxiqt',
+//     SE: 'https://gen.sendtric.com/countdown/gwf2vg71l8',
+//     HU: 'https://gen.sendtric.com/countdown/qt1r7trg7u',
+//     DK: 'https://gen.sendtric.com/countdown/pnte8osmq9',
+//     CZ: 'https://gen.sendtric.com/countdown/4exrxd8via',
+//     FI: 'https://gen.sendtric.com/countdown/2db8tzzosw',
+//     NO: 'https://gen.sendtric.com/countdown/vgah7mf9hy',
+//     SK: 'https://gen.sendtric.com/countdown/0vt1w09jx7',
+//     BENL: 'https://gen.sendtric.com/countdown/3rvc02tymo',
+//     BEFR: 'https://gen.sendtric.com/countdown/sdnqmw4n9c',
+//     RO: 'https://gen.sendtric.com/countdown/08c4nq32kk',
+//   },
+// };
+
+export default new entities.Campaign({
+  startId: 43950,
+  name: 'Met Gala',
+  date: '21.05.2026',
+  issueCardId: 481309,
+  lpId: 30279,
+  alarm: {
+    isActive: false,
+  },
+  isArchive: false,
+  optimizeImg: true,
+  figmaUrl: 'https://www.figma.com/design/kzJhNvGUJi9X1qQIJLrT9N/2026.05.21---Newsletter---Met-Gala-2026--Copy-?node-id=11167-404&t=ti9yMmZehVNyn6Pc-1',
+  templates: [
+    {
+      name: 'Newsletter',
+      type: types.NEWSLETTER,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.NS,
+      translationsSpreadsheet: campaignTranslationsSheet,
+      background: '#FFF0EA',
+      color: '#000000',
+      wrapper: types.WRAPPER,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      // timer: timer,
+      intro: {
+        cta: {
+          spaceAfter: 'newsletterBottom80px',
+        },
+        color: '#000000', // unused
+        backgroundColor: '#FFF0EA', // unused
+        alignment: 'center',
+        type: 'paragraph',
+      },
+    },
+    {
+      name: 'Landing',
+      type: types.LANDINGPAGE,
+      template: templates.Thursday, // User should change this
+      css: types.CSS.LP,
+      background: '#FFF0EA',
+      color: '#000000',
+      translationsSpreadsheet: campaignTranslationsSheet,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+      // timer: timer,
+      intro: {
+        cta: {
+          spaceAfter: 'newsletterBottom80px',
+        },
+        color: '#000000', // unused
+        backgroundColor: '#FFF0EA', // unused
+        alignment: 'center',
+        type: 'paragraph',
+      },
+    },
+  ],
+});
