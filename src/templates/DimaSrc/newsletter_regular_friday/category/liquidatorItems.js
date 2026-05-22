b88fa483ee668366869f3d01e7c558f079ccdf7a
ShadowDrake21 <dimka670020040@gmail.com>
2026-05-22 11:49:04 +0200

    feat: Dima's Liquidator NSLT (part 1)

---
+++ src/templates/DimaSrc/newsletter_regular_friday/category/liquidatorItems.js
@@ -0,0 +1,62 @@
+import { Space } from '../components/Space';
+
+export const liquidatorItems = ({
+  category,
+  color,
+  background,
+}) => {
+  let productsInnerHtml = '';
+  console.log('liquidatorItems', category);
+
+  let items = category.items;
+
+  if (Array.isArray(items) && items.length > 0) {
+    productsInnerHtml += `
+    <tr>
+      <td style="color: ${color}; background: ${background};">
+        <table cellspacing="0" cellpadding="0" border="0" width="100%" style="max-width: 650px;">
+          <tr>`;
+
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+
+      // Image TD
+      productsInnerHtml += `
+        <td style="width: 25%; vertical-align: top;" width="25%">
+          ${item ? `
+            <img 
+              alt="Item" 
+              src="${item.src.src || item.src}" 
+              style="width:100%; max-width: 100%; display:block;" 
+              loading="lazy"
+            >
+          ` : ''}
+        </td>`;
+
+      // Add spacer TD (except after the last image)
+      if (i < items.length - 1) {
+        productsInnerHtml += `
+          <td style="width: 10px;" width="10">
+            &nbsp;&nbsp;
+          </td>`;
+      }
+    }
+
+    productsInnerHtml += `
+          </tr>
+        </table>
+      </td>
+    </tr>`;
+
+    // Add space after if needed
+    if (category.spaceAfter) {
+      productsInnerHtml += `${Space({
+        insideTr: true,
+        className: category.spaceAfter,
+        backgroundColor: background
+      })}`;
+    }
+  }
+
+  return productsInnerHtml;
+};
