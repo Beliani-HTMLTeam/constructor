34026d59db50f8f9527008dcef9c059b4d906b0b
kkazzann <kamil.kazaniecki@beliani.net>
2026-08-06 10:20:42 +0200

    feat: add 26.08 camp

---
+++ campaigns/KamilK/059_2026-08-26-armchairs.js
@@ -0,0 +1,336 @@
+// Campaign generated from form
+const campaignTranslationsSheet = '2026::26.08.26 - Armchairs';
+
+const tableQueries = [
+  {
+    tableRange: '16',
+    name: 'TopImageTitle',
+  },
+  {
+    tableRange: '18:21',
+    name: 'paragraphs',
+  },
+  {
+    tableRange: '22:23',
+    name: 'condition',
+  },
+];
+
+const links = {
+  TopImageTitle_href: translateLink({ value: 'content/lp26-08-26' }),
+  TopImageTitle_src: translateImage({ value: '20260826_01.png' }),
+
+  // TopImage_src: getImageUrl('20260826_Top.png', true),
+  // TopImage_href: 'https://www.beliani.ch/rugs/low-pile-rugs/',
+
+  Banner_1: translateLink({ value: 'content/lp26-08-13' }),
+  Banner_1_Image: translateImage({ value: '20260813b.png' }),
+
+  Banner_2: translateLink({ value: 'content/lp26-08-12' }),
+  Banner_2_Image: translateImage({ value: '20260812b.png' }),
+};
+
+const TopImageTitle_data = {
+  color: '#000000',
+  backgroundColor: '#FFEFD9',
+  type: 'singleLine',
+};
+
+const categories = [
+  {
+    name: 'Fabric Armchairs',
+    src: getImageUrl('20260826_Cat00.jpg', true),
+    href: 'https://www.beliani.ch/armchairs/fabric-armchairs/',
+    background: '#FFEFD9',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: 0,
+    title: {
+      position: 'afterImg',
+      show: true,
+      align: 'center',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'center',
+      // spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      align: 'center',
+      prices: true,
+      name: true,
+    },
+    products: [
+      // SANDET 
+      {
+        id: '734068',
+        src: getImageUrl('20260826_Pic01.png', true),
+      },
+      // BUREA 
+      {
+        id: '733099',
+        src: getImageUrl('20260826_Pic02.png', true),
+      },
+      // RANDERS 
+      {
+        id: '724314',
+        src: getImageUrl('20260826_Pic03.png', true),
+      },
+      // HAMINA 
+      {
+        id: '734204',
+        src: getImageUrl('20260826_Pic04.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Reclining Armchairs',
+    src: getImageUrl('20260826_Cat10.jpg', true),
+    href: 'https://www.beliani.ch/armchairs/reclining-armchairs/',
+    background: '#F6E7E6',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: 0,
+    title: {
+      position: 'afterImg',
+      show: true,
+      align: 'center',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'center',
+      // spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      align: 'center',
+      prices: true,
+      name: true,
+    },
+    products: [
+      // FEBE 
+      {
+        id: '809049',
+        src: getImageUrl('20260826_Pic11.png', true),
+      },
+      // GERAS
+      {
+        id: '649611',
+        src: getImageUrl('20260826_Pic12.png', true),
+      },
+      // ROYSTON 
+      {
+        id: '423660',
+        src: getImageUrl('20260826_Pic13.png', true),
+      },
+      // PRIME 
+      {
+        id: '516155',
+        src: getImageUrl('20260826_Pic14.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Rocking Chairs',
+    src: getImageUrl('20260826_Cat20.jpg', true),
+    href: 'https://www.beliani.ch/armchairs/rocking-chairs/',
+    background: '#FFEFD9',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: 0,
+    title: {
+      position: 'afterImg',
+      show: true,
+      align: 'center',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'center',
+      // spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      align: 'center',
+      prices: true,
+      name: true,
+    },
+    products: [
+      // SKOVDE 
+      {
+        id: '667179',
+        src: getImageUrl('20260826_Pic21.png', true),
+      },
+      // OULU 
+      {
+        id: '448738',
+        src: getImageUrl('20260826_Pic22.png', true),
+      },
+      // ANASET 
+      {
+        id: '496883',
+        src: getImageUrl('20260826_Pic23.png', true),
+      },
+      // KVELIA 
+      {
+        id: '607138',
+        src: getImageUrl('20260826_Pic24.png', true),
+      },
+    ],
+  },
+  {
+    name: 'Hanging chairs',
+    src: getImageUrl('20260826_Cat30.jpg', true),
+    href: 'https://www.beliani.ch/living-room-furniture/armchairs/hanging-chairs/',
+    background: '#F6E7E6',
+    color: '#000000',
+    type: 'grid',
+    cta: true,
+    paddingTop: 0,
+    spaceAfter: 'newsletterBottom40px',
+    title: {
+      position: 'afterImg',
+      show: true,
+      align: 'center',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: true,
+      align: 'center',
+      // spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      align: 'center',
+      prices: true,
+      name: true,
+    },
+    products: [
+      // TOLLO 
+      {
+        id: '188801',
+        src: getImageUrl('20260826_Pic31.png', true),
+      },
+      // BONEA 
+      {
+        id: '311652',
+        src: getImageUrl('20260826_Pic32.png', true),
+      },
+      // ALBA 
+      {
+        id: '248862',
+        src: getImageUrl('20260826_Pic33.png', true),
+      },
+      // PINETO 
+      {
+        id: '188775',
+        src: getImageUrl('20260826_Pic34.png', true),
+      },
+    ],
+  },
+  {
+    name: 'This may also interest you',
+    // src: getImageUrl('20260826_Cat30.png', true),
+    // href: 'https://www.beliani.ch/home-accessories/kitchenware-tableware/',
+    background: '#FFFFFF',
+    color: '#000000',
+    type: 'categorytiles',
+    cta: false,
+    paddingTop: 0,
+    spaceAfter: 0,
+    title: {
+      className: 'newsletterAditionalTitle',
+      align: 'center',
+      show: true,
+      spaceBefore: 'newsletterBottom40px',
+      // spaceAfter: 'newsletterBottom35px',
+    },
+    paragraph: {
+      show: false,
+      align: 'center',
+      spaceBefore: 'newsletterBottom35px',
+      spaceAfter: 'newsletterBottom35px',
+    },
+    product: {
+      align: 'center',
+    },
+    tiles: [
+      {
+        name: 'Textiles',
+        src: getImageUrl('20260826_Pic41.png', true),
+        href: 'https://www.beliani.ch/living-room-furniture/textiles/',
+      },
+      {
+        name: 'Rugs',
+        src: getImageUrl('20260826_Pic42.png', true),
+        href: 'https://www.beliani.ch/living-room-furniture/rugs/',
+      },
+      {
+        name: 'Pouffes & stools',
+        src: getImageUrl('20260826_Pic43.png', true),
+        href: 'https://www.beliani.ch/living-room-furniture/stools/',
+      },
+      {
+        name: 'Lighting',
+        src: getImageUrl('20260826_Pic44.png', true),
+        href: 'https://www.beliani.ch/living-room-furniture/lighting/',
+      },
+    ],
+  },
+];
+
+export default new entities.Campaign({
+  startId: 47058,
+  name: 'Armchairs',
+  date: '26.08.2026',
+  issueCardId: 518997,
+  lpId: 31901,
+  version: 'new',
+  alarm: {
+    isActive: false,
+  },
+  isArchive: false,
+  optimizeImg: true,
+  figmaUrl: 'https://www.figma.com/design/2l4Gehsxz76Agjvmgv8THX/',
+  templates: [
+    {
+      background: '#FFEFD9',
+      color: '#000000',
+      template: templates.Thursday, // User should change this
+
+      css: types.CSS.NS,
+      name: 'Newsletter',
+      type: types.NEWSLETTER,
+      translationsSpreadsheet: campaignTranslationsSheet,
+      wrapper: types.WRAPPER,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+    },
+    {
+      background: '#FFEFD9',
+      color: '#000000',
+      template: templates.Thursday, // User should change this
+
+      css: types.CSS.LP,
+      name: 'Landing',
+      type: types.LANDINGPAGE,
+      translationsSpreadsheet: campaignTranslationsSheet,
+      TopImageTitle_data: TopImageTitle_data,
+      categories: categories,
+      links: links,
+      tableQueries: tableQueries,
+    },
+  ],
+});
