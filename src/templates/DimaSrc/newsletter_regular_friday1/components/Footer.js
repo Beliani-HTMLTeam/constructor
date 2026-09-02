57329169da1889d2d9365b765d0720ce8f72c7ac
ShadowDrake21 <dimka670020040@gmail.com>
2026-09-02 13:21:13 +0200

    feat: friday new assembly

--- src/templates/DimaSrc/newsletter_regular_friday1/components/Footer.js
+++ src/templates/DimaSrc/newsletter_regular_friday1/components/Footer.js
@@ -10,11 +10,42 @@ import {
   workBanner,
 } from './utils/footer/components';
 
+function shouldUseNewAssembly(cDate) {
+  const parts = cDate.split('.');
+
+  const day = Number(parts[0]);
+  const month = Number(parts[1]);
+  const year = Number(parts[2]);
+
+  const campaignDate = new Date(year, month - 1, day);
+  const cutoffDate = new Date(2026, 8, 14);
+
+  return campaignDate > cutoffDate;
+}
+
 const Footer = ({ getFooter, getCategoryLink, getCategoryTitle, queries, country, type, id, hasSmallTilesCategory }) => {
+  const campaignDate = date || selectedCampaign?.date || getState('selectedCampaign')?.date;
+  const newAssembly = shouldUseNewAssembly(campaignDate);
+
+  const assemblyBanner = newAssembly
+    ? {
+        src: getFooter('Assembly src new'),
+        href: getFooter('Assembly href NEW'),
+      }
+    : {
+        src: ['AT', 'PL', 'FR', 'UK'].includes(country) ? getFooter('Delivery src') : getFooter('Asembly src'),
+        href: getFooter('Asembly href'),
+      };
   return FooterComponent(
     {
       id,
-      assembly: assembly({ getFooter, country }),
+      assembly:{
+        src: ['AT', 'PL', 'FR', 'UK'].includes(country)
+          ? getFooter('Delivery src')
+          : getFooter('Asembly src'),
+        href: getFooter('Asembly href'),
+        exclude: ['CHIT'].includes(country),
+      },
 
       workBanner: workBanner({ getFooter, country }),
 
