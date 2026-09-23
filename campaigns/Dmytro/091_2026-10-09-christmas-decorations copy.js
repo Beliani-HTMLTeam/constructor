0be52fa4b57957e721b35320e5a0a98a89ae0e59
ShadowDrake21 <dimka670020040@gmail.com>
2026-09-23 09:53:08 +0200

    feat: reminder 2

--- campaigns/Dmytro/091_2026-10-09-christmas-decorations copy.js
+++ campaigns/Dmytro/091_2026-10-09-christmas-decorations copy.js
@@ -1,161 +1,162 @@
 
 // Campaign generated from form
-const campaignTranslationsSheet = '2026::09.10.26 - Christmas Categories';
+const campaignTranslationsSheet = '2026::Voucher - 30.10.26 - Halloween Cashback Reminder';
 
 const tableQueries = [
   {
-    "tableRange": "16:17",
+    "tableRange": "22:23",
     "name": "TopImageTitle"
   },
   {
-    "tableRange": "19",
-    "name": "intro"
+    "tableRange": "25:27",
+    "name": "offerPart"
   },
   {
-    "tableRange": "20:21",
+    "tableRange": "32:34",
     "name": "condition"
   },
   {
-    "tableRange": "25:26",
-    "name": "timer"
+    "tableRange": "30",
+    "name": "codeCTA",
+  },
+  {
+    "tableRange": "28",
+    "name": "code",
   }
 ];
 
 const links = {
-  TopImage_src:  translateImage({ value: '20261009_img.png' }),
-  TopImage_href: translateLink({ value: 'content/lp26-10-09' }),
-  Banner_1: translateLink({ value: 'content/lp26-09-30' }),
-  Banner_1_Image: translateImage({ value: '20260930b.png' }),
-  Banner_2: translateLink({ value: 'content/lp26-10-01' }),
-  Banner_2_Image: translateImage({ value: '20261001b.png' }),
-  Timer_href: translateLink({ value: 'content/lp26-10-06' }),
-  Intro_cta_href: "https://www.beliani.ch/christmas-shop/christmas-accessories/"
+  code_href: translateLink({ value: 'content/lp26-07-03' }),
+  TopImageTitle_href: translateLink({ value: 'content/lp26-07-03' }),
+  TopImageTitle_src: DimasTranslateImage({ value: '20260703_01.png', folder: '20260703', placeholderPosition: '47' }),
+  TopImage_src: getImageUrl('20260703/20260703topgif.gif', true),
+  TopImage_href: translateLink({ value: 'content/lp26-07-03' }),
+  Banner_1: translateLink({ value: 'content/lp26-06-26' }),
+  Banner_1_Image: translateImage({ value: '20260626b.png' }),
+  Banner_2: translateLink({ value: 'content/lp26-06-25' }),
+  Banner_2_Image: translateImage({ value: '20260625b.png' }),
+  Timer_href: translateLink({ value: 'content/lp26-07-03' }),
+  ShopCTA: translateLink({ value: ' ' }),
 };
 
 const TopImageTitle_data = {
-  color: '#ffffff',
-  backgroundColor: '#FF2F00',
+  color: '#000000',
+  backgroundColor: '#FFDEB3',
   type: 'twoSameLines',
 };
 
-const intro_data = {
-  color: "#750000",
-  backgroundColor: "#F6E7E6",
-  alignment: "center",
-  type: "paragraph",
-  cta: {
-    overrides: {
-    },
-    spaceAfter: 'newsletterBottom35px',
-  }
-}
-
-const timer ={
-  position: 'outsideTopImageTitle',
-  deadline: '2026-10-11',
-  freebies: getImageUrl('20261009free.png', true)
-}
+const timer = {
+  position: "insideTopImageTitle",
+  image: {
+    UK: "https://gen.sendtric.com/countdown/a51vgq707l",
+    CHDE: "https://gen.sendtric.com/countdown/zp3gl4v7v4",
+    CZ: "https://gen.sendtric.com/countdown/xkzhx4dwlz",
+    DK:"https://gen.sendtric.com/countdown/g3rt5mia8i",
+    DE:"https://gen.sendtric.com/countdown/oow6s3jzjb",
+    NL:"https://gen.sendtric.com/countdown/5osdtw6hdx",
+    FI:"https://gen.sendtric.com/countdown/78rq33ycgz",
+    NO:"https://gen.sendtric.com/countdown/6mb1l2qsq4",
+    SE:"https://gen.sendtric.com/countdown/ab5kx7rx5z",
+    PL:"https://gen.sendtric.com/countdown/jj6raoo8qm",
+    PT:"https://gen.sendtric.com/countdown/ieoedf0n17",
+    SK:"https://gen.sendtric.com/countdown/ru0c46okn4",
+    ES:"https://gen.sendtric.com/countdown/zy9c2p0zna",
+    FR:"https://gen.sendtric.com/countdown/kgehl003a8",
+    HU:"https://gen.sendtric.com/countdown/8tkcegxv6p",
+    IT:"https://gen.sendtric.com/countdown/7cvpulq7c1",
+    BENL:"https://gen.sendtric.com/countdown/bokbx2hpv9",
+    BEFR:"https://gen.sendtric.com/countdown/u44ursrnzc",
+    CHFR:"https://gen.sendtric.com/countdown/5em6hpvmn0",
+    AT:"https://gen.sendtric.com/countdown/9sniel8qo7",
+    RO:"https://gen.sendtric.com/countdown/oazgmhksx3"
+  },
+};
 
 const categories = [
   {
-    type: 'smallgrid',
-    background: '#F6E7E6',
-    paddingTop: 25,
-    title: {
-      show: true,
-      align: 'center',
-      spaceAfter: 'newsletterBottom25px',
-      color: '#750000',
-      source: 'Shop by category',
-      className: 'newsletterSmallgridCategoryTitle',
-    },
-    paragraph: {
-      show: false,
-      spaceAfter: 'newsletterBottom0px',
-    },
-    topTitleColor: "#000000",
-    subtitle: {
-      show: true,
-      align: 'center',
-      spaceAfter: 'newsletterBottom10px',
-      color: '#750000',
-    },
-    color: "#750000",
-    ctaColor: '#750000',
-    products: [
-      {
-        name: 'Christmas Trees',
-        overrides: {
-        },
-        src: getImageUrl('20261009Category1.png', true),
-        href: 'https://www.beliani.ch/christmas-shop/christmas-accessories/christmas-tree/',
-      },
-      {
-        name: 'Tree Decorations',
-        overrides: {
-        },
-       
-        src: getImageUrl('20261009Category2.png', true),
-        href: 'https://www.beliani.ch/christmas-shop/christmas-accessories/christmas-tree-decorations/',
-      },
-      {
-        name: 'Christmas Tree Collars',
-        overrides: {
-        },
-        src: getImageUrl('20261009Category3.png', true),
-        href: 'https://www.beliani.ch/christmas-shop/christmas-accessories/christmas-tree-collars/',
-      },
-      {
-        name: 'Decorations',
-        overrides: {
-        },
-        src: getImageUrl('20261009Category4.png', true),
-        href: 'https://www.beliani.ch/christmas-shop/christmas-accessories/decorations/',
-      },
-      {
-        name: 'LED Decor',
-        overrides: {
-        },
-        src: getImageUrl('20261009Category5.png', true),
-        href: 'https://www.beliani.ch/christmas-shop/christmas-accessories/christmas-outdoor-decor/',
-      },
-      {
-        name: 'Wreaths',
-        overrides: {
-        },
-        src: getImageUrl('20261009Category6.png', true),
-        href: 'https://www.beliani.ch/christmas-shop/christmas-accessories/christmas-wreaths/',
-      },
-      {
-        name: 'Garlands',
-        overrides: {
-        },
-        src: getImageUrl('20261009Category7.png', true),
-        href: 'https://www.beliani.ch/christmas-shop/christmas-accessories/christmas-garland/',
-      },
-      {
-        name: 'Textiles',
-        overrides: {
-        },
-        src: getImageUrl('20261009Category8.png', true),
-        href: 'https://www.beliani.ch/christmas-shop/christmas-accessories/christmas-textiles/',
-      },
-      {
-        name: 'Lights',
-        overrides: {
-        },
-        src: getImageUrl('20261009Category9.png', true),
-        href: 'https://www.beliani.ch/christmas-shop/christmas-accessories/christmas-lights/',
-      },
-      {
-        name: 'Candle Holders',
-        overrides: {
-        },
-        src: getImageUrl('20261009Category10.png', true),
-        href: 'https://www.beliani.ch/christmas-shop/christmas-accessories/christmas-candle-holders/',
-      }
-    ]
-  }
+    name: 'Outdoor',
+    src: DimasTranslateImage({ value: '_outdoor.png', folder: 'category_tiles/ff2f00/ffffff', placeholderPosition: '67' }),
+    href: 'https://www.beliani.ch/garden-furniture/outdoor-furniture/ ',
+    type: 'tilesWithoutProducts', // make it by hand (of from old constructor)
+  },
+  {
+    name: 'Sofas',
+    src: DimasTranslateImage({ value: '_sofas.png', folder: 'category_tiles/ff2f00/ffffff', placeholderPosition: '67' }),
+    href: 'https://www.beliani.ch/sofas/',
+    type: 'tilesWithoutProducts', // make it by hand (of from old constructor)
+  },
+  {
+    name: 'Beds',
+    src: DimasTranslateImage({ value: '_beds.png', folder: 'category_tiles/ff2f00/ffffff', placeholderPosition: '67' }),
+    href: 'https://www.beliani.ch/beds/',
+    type: 'tilesWithoutProducts', // make it by hand (of from old constructor)
+  },
+  {
+    name: 'Armchairs',
+    src: DimasTranslateImage({ value: '_armchairs.png', folder: 'category_tiles/ff2f00/ffffff', placeholderPosition: '67' }),
+    href: 'https://www.beliani.ch/armchairs/',
+    type: 'tilesWithoutProducts', // make it by hand (of from old constructor)
+  },
+  {
+    name: 'Chairs',
+    src: DimasTranslateImage({ value: '_chairs.png', folder: 'category_tiles/ff2f00/ffffff', placeholderPosition: '67' }),
+    href: 'https://www.beliani.ch/chairs/',
+    type: 'tilesWithoutProducts', // make it by hand (of from old constructor)
+  },
+  {
+    name: 'Tables',
+    src: DimasTranslateImage({ value: '_tables.png', folder: 'category_tiles/ff2f00/ffffff', placeholderPosition: '67' }),
+    href: 'https://www.beliani.ch/tables/',
+    type: 'tilesWithoutProducts', // make it by hand (of from old constructor)
+  },
+  {
+    name: 'Storage',
+    src: DimasTranslateImage({ value: '_storage.png', folder: 'category_tiles/ff2f00/ffffff', placeholderPosition: '67' }),
+    href: 'https://www.beliani.ch/storage/',
+    type: 'tilesWithoutProducts', // make it by hand (of from old constructor)
+  },
+  {
+    name: 'Kids Room',
+    src: DimasTranslateImage({ value: '_kids_room.png', folder: 'category_tiles/ff2f00/ffffff', placeholderPosition: '67' }),
+    href: 'https://www.beliani.ch/children-room/',
+    type: 'tilesWithoutProducts', // make it by hand (of from old constructor)
+  },
+  {
+    name: 'Textiles',
+    src: DimasTranslateImage({ value: '_textiles.png', folder: 'category_tiles/ff2f00/ffffff', placeholderPosition: '67' }),
+    href: 'https://www.beliani.ch/textiles/',
+    type: 'tilesWithoutProducts', // make it by hand (of from old constructor)
+  },
+  {
+    name: 'Lighting',
+    src: DimasTranslateImage({ value: '_lighting.png', folder: 'category_tiles/ff2f00/ffffff', placeholderPosition: '67' }),
+    href: 'https://www.beliani.ch/lighting/',
+    type: 'tilesWithoutProducts', // make it by hand (of from old constructor)
+  },
+  {
+    name: 'Bathtubs',
+    src: DimasTranslateImage({ value: '_bathtubs.png', folder: 'category_tiles/ff2f00/ffffff', placeholderPosition: '67' }),
+    href: 'https://www.beliani.ch/bathtubs-hot-tubs/',
+    type: 'tilesWithoutProducts', // make it by hand (of from old constructor)
+  },
+  {
+    name: 'Desks',
+    src: DimasTranslateImage({ value: '_desks.png', folder: 'category_tiles/ff2f00/ffffff', placeholderPosition: '67' }),
+    href: 'https://www.beliani.ch/office-furniture/desks-eng/',
+    type: 'tilesWithoutProducts', // make it by hand (of from old constructor)
+  },
+  {
+    name: 'Rugs',
+    src: DimasTranslateImage({ value: '_rugs.png', folder: 'category_tiles/ff2f00/ffffff', placeholderPosition: '67' }),
+    href: 'https://www.beliani.ch/rugs/',
+    type: 'tilesWithoutProducts', // make it by hand (of from old constructor)
+  },
+  {
+    name: 'Accessories',
+    src: DimasTranslateImage({ value: '_accessories.png', folder: 'category_tiles/ff2f00/ffffff', placeholderPosition: '67' }),
+    href: 'https://www.beliani.ch/home-accessories/accessories-decor/',
+    type: 'tilesWithoutProducts', // make it by hand (of from old constructor)
+  },
 ];
 
 export default new entities.Campaign({
