e0368029e8235e20f12199dda1d3655313b4a95a
Krzysztof <krzysieq95.mail@gmail.com>
2026-09-24 08:58:04 +0200

    feat: update monday regular template and css

--- src/templates/JakubSrc/monday_newsletter/components/CTA.js
+++ src/templates/JakubSrc/monday_newsletter/components/CTA.js
@@ -11,6 +11,7 @@ const CTA = ({
   bg = '#750000',
   borderColor = bg,
   borderWidth = '15px 45px',
+  elemClass = 'newsletterCta'
 }) => {
   const tableAttributes = `cellspacing="0" cellpadding="0" border="0" width="100%"`;
 
@@ -19,7 +20,7 @@ const CTA = ({
   // prettier-ignore
   html += variant === 'button'
     ? `<table role="presentation" align="${align}" cellspacing="0" cellpadding="0" border="0" style="width:auto;margin:0 auto;border-collapse:collapse;"><tr><td align="${align}" bgcolor="${bg}" style="background-color:${bg};"><a href="${href}" style="display:inline-block;color:${color} !important;background-color:${bg};text-decoration:none !important;border-style:solid;border-width:${borderWidth};border-color:${borderColor};font-size:16px;line-height:20px;font-weight:600;text-transform:uppercase;"><span style="color:${color};">${text}</span></a></td></tr></table>`
-    : `<a style="color: ${color}; text-decoration: underline;" href="${href}"><span class="newsletterCta">${text}</span></a>`;
+    : `<a style="color: ${color}; text-decoration: underline;" href="${href}"><span class="${elemClass}">${text}</span></a>`;
 
   if (insideTr) {
     html = `<tr><td ${tdClass ? `class="${tdClass}"` : ''} align=${align}>${html}</td></tr>`;
